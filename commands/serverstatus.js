661610fc61b5d56383a0dc6fd319cd7958ad622e
Ken_Cir0909 <test@watanabekentonoMacBook-Air.local>
2021-01-17 20:29:55 +0900

    serverstatuscommand追加

---
+++ commands/serverstatus.js
@@ -0,0 +1,43 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const util = require('minecraft-server-util');
+
+module.exports = {
+    info: {
+        name: "ping",
+        description: "BotのPing値とメモリ使用率を表示",
+        usage: "",
+        aliases: [""],
+        botownercommand: false,
+        botadmincommand: false
+    },
+/**
+ * @param {Message} message
+ */
+    run: async function(client, message, args) {
+        util.statusBedrock('126.235.33.140')
+        .then((result) => {
+            message.channel.send(
+              new MessageEmbed()
+              .setTitle('💩うんこサーバーの現在の状態💩')
+              .addField('IPアドレス', result.host)
+              .addField('ポート', result.port)
+              .addField('サーバーのバージョン', result.version)
+              .addField('現在参加中のメンバー', `${result.onlinePlayers}/${result.maxPlayers}人`)
+              .setImage('https://media.discordapp.net/attachments/800317829962399795/800317877168373760/UnkoServerkoiyo.png')
+              .setColor('RANDOM')
+              .setTimestamp()
+            );
+        })
+        .catch((error) => {
+            message.channel.send(
+            new MessageEmbed()
+            .setTitle('💩うんこサーバーの現在の状態💩')
+            .setDescription('うんこサーバーは現在落ちてます')
+            .setImage('https://media.discordapp.net/attachments/800317829962399795/800317874614829086/setumeisitekudasai.jpg')
+            .setColor('RANDOM')
+            .setTimestamp()
+          );
+          console.error(error)
+        });
+    },
+};
