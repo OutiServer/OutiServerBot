198e88b565315bdf6871a2746f21a020bb258f0a
Ken_Cir0909 <test@watanabekentonoMacBook-Air.local>
2021-01-17 11:31:21 +0900

    Banメッセージ追加2

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -7,7 +7,7 @@ const { Client, Guild, User } = require('discord.js');
  */
 
 module.exports = async (client, guild, user) => {
-    const replys = ['がBANされましたwwwwwwwwwwwww', 'はシベリア旅行に行った', 'は BABANBABANBANBA BABANBABANBANBAN された', 'は下水処理場へ流れていった。', 'は豚箱に送られた', 'は粛清された', 'は北に拉致された'];
+    const replys = ['がBANされましたwwwwwwwwwwwww', 'はシベリア旅行に行った', 'は BABANBABANBANBA BABANBABANBANBAN された', 'は下水処理場へ流れていった。', 'は豚箱に送られた', 'は粛清された', 'は北に拉致された', 'は自粛した'];
     let random = Math.ceil( Math.random() * replys.length);
     client.channels.cache.get('706459931351711775').send(user.tag+replys[random]);
 }
