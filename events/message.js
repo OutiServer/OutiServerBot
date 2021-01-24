2ad0ecb1db1bc50321129bc431c9f6ee4dc47df9
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-01-24 21:42:53 +0900

    修正

--- events/message.js
+++ events/message.js
@@ -77,7 +77,7 @@ module.exports = async (client, message) => {
     const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.info.aliases && cmd.info.aliases.includes(command));
-    if (!cmd || cmd.info.botownercommand && process.env.OWNERID !== message.author.id || cmd.info.bbotadmincommand && !message.member.roles.cache.has('771015602180587571') && message.guild.id === '706452606918066237'){
+    if (!cmd || cmd.info.botownercommand && process.env.OWNERID !== message.author.id || cmd.info.botadmincommand && !message.member.roles.cache.has('771015602180587571') && message.guild.id === '706452606918066237'){
       message.reply('そんなコマンドないで。😉').then( msg => {
         msg.delete({ timeout: 5000 });
       });
