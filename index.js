d7e2a6f20450da23d4c978f9a289508c32831049
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-02-26 21:30:01 +0900

    error-log

--- index.js
+++ index.js
@@ -217,14 +217,12 @@ const textclient = new textToSpeech.TextToSpeechClient({
 })().catch((e) => console.error(e));
 
 process.on('unhandledRejection', (reason, promise) => {
-  console.error(`${reason}`);
-  client.guilds.cache.get('794380572323086358').channels.create(reason, { parent: '814514296049369159', position: 0, type: 'text' })
-    .then(channel => channel.send(
-      new MessageEmbed()
-        .setDescription(reason)
-        .setColor('RED')
-        .setTimestamp()
-    ));
+  client.users.cache.get('714455926970777602').send(
+    new MessageEmbed()
+      .setDescription(`エラー内容: ${reason}`)
+      .setColor('RANDOM')
+      .setTimestamp()
+  );
+  console.error(`エラー内容: ${reason}`);
 });
-
 client.login();
