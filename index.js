318a63d87bb75b0c808586e45c593d0f76003d08
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-02-27 15:57:48 +0900

    読み上げ死亡

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const { readdir } = require("fs");
 const { Client, Intents, Collection, MessageEmbed, MessageAttachment } = require('discord.js');
-const textToSpeech = require('@google-cloud/text-to-speech');
-const { Readable } = require('stream');
 const cron = require('node-cron');
 const util = require('minecraft-server-util');
 const SQLite = require("better-sqlite3");
@@ -148,73 +146,6 @@ cron.schedule('* * * * *', () => {
         });
     })
 });
-
-async function textToSpeechReadableStream(text) {
-  const request = {
-    input: { text },
-    voice: {
-      languageCode: 'ja-JP',
-      name: 'ja-JP-Wavenet-A'
-    },
-    audioConfig: {
-      audioEncoding: 'OGG_OPUS',
-      speakingRate: 1.2
-    }
-  };
-
-  const [response] = await textclient.synthesizeSpeech(request);
-  const stream = new Readable({ read() { } });
-  stream.push(response.audioContent);
-
-  return stream;
-}
-const textclient = new textToSpeech.TextToSpeechClient({
-  credentials: {
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-  }
-});
-(async function main() {
-  const discordClient = new Client({
-    messageCacheMaxSize: 20,
-    messageSweepInterval: 30
-  });
-  discordClient.on('voiceStateUpdate', () => {
-    const conn = discordClient.voice.connections.get(process.env.DISCORD_GUILD_ID);
-    if (conn && conn.channel && conn.channel.members.array().length < 2) {
-      conn.disconnect();
-    }
-  });
-  discordClient.on('message', async (message) => {
-    if (message.author.bot || !message.guild) return
-    const guild = message.guild;
-    const channel = message.member.voice.channel;
-    if (
-      !message.member.voice.selfMute || guild.id !== process.env.DISCORD_GUILD_ID ||
-      !channel || message.channel.id !== process.env.DISCORD_SOURCE_CHANNEL_ID
-    ) {
-      return;
-    }
-    const text = message
-      .content
-      .replace(/https?:\/\/\S+/g, 'URL省略')
-      .replace(/<a?:.*?:\d+>/g, '絵文字省略')
-      .replace(/<@!?.*?>/g, 'メンション省略')
-      .replace(/<#.*?>/g, 'メンション省略')
-      .replace(/<@&.*?>/g, 'メンション省略')
-      .slice(0, 50);
-    if (!text) { return; }
-    if (channel.members.array().length < 1) { return; }
-    const currentConnection = discordClient.voice.connections.get(process.env.DISCORD_GUILD_ID);
-    const shouldMove = !currentConnection || currentConnection.channel.id !== channel.id;
-    const conn = shouldMove ? await channel.join() : currentConnection;
-    conn.play(await textToSpeechReadableStream(text), { highWaterMark: 6, bitrate: 'auto' })
-  });
-  discordClient.once('ready', () => {
-    console.log('[INFO] Connected to Discord successfully!');
-  });
-  discordClient.login(process.env.DISCORD_BOT_TOKEN);
-})().catch((e) => console.error(e));
 
 process.on('unhandledRejection', (reason, promise) => {
   console.error(reason);
