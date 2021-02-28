23886e4299b9ca0dfcbe9d55db7295f8ca4facb1
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-02-28 20:35:44 +0900

    バックアップの場所変更

--- events/ready.js
+++ events/ready.js
@@ -83,7 +83,7 @@ module.exports = async (client) => {
   const time = new Date();
   sql.backup(`${time}.db`)
     .then(() => {
-      client.guilds.cache.get('794380572323086358').channels.create(time, { parent: '814406269074538496', position: 0, type: 'text' })
+      client.guilds.cache.get('775952658779209770').channels.create(time, { parent: '815547487506137129', position: 0, type: 'text' })
         .then(channel => {
           channel.send(new MessageAttachment(`${time}.db`));
         });
