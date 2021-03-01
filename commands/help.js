ca38d1214c1fd2fb68701f37a7bc97a7ce6bf383
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-01 15:41:47 +0900

    help修正

--- commands/help.js
+++ commands/help.js
@@ -27,13 +27,13 @@ module.exports = {
             .setTimestamp(),
           new MessageEmbed()
             .setTitle(`${client.user.tag} help2ページ目(通貨コマンド)`)
-            .setDescription('```' + `${process.env.PREFIX}money :所持うんコインを確認するコマンド、第一引数にユーザーをメンションするかIDを入れるとその人のうんコインを表示します\n${process.env.PREFIX}moneyrank: うんコイン所持数ランキング\n${process.env.PREFIX}give [ユーザーをメンションまたはid] [付与するうんコイン]: 指定したユーザーにうんコインを自分の分から引いて付与する\n${process.env.PREFIX}add [ユーザーをメンションまたはid] [付与するうんコイン]: 指定したユーザーにうんコインを付与\n${process.env.PREFIX}remove [ユーザーをメンションまたはid] [剥奪するうんコイン]: 指定したユーザーからうんコインを剥奪するコマンド\n${process.env.PREFIX}reset [ユーザーをメンションまたはid]: 指定したユーザーのうんコインをリセット(初期値)にする` + '```')
+            .setDescription('```' + `${process.env.PREFIX}money :所持うんコインを確認するコマンド、第一引数にユーザーをメンションするかIDを入れるとその人のうんコインを表示します\n${process.env.PREFIX}moneyrank: うんコイン所持数ランキング\n${process.env.PREFIX}give [ユーザーをメンションまたはid] [付与するうんコイン]: 指定したユーザーにうんコインを自分の分から引いて付与する\n${process.env.PREFIX}add [ユーザーをメンションまたはid] [付与するうんコイン]: 指定したユーザーにうんコインを付与\n${process.env.PREFIX}remove [ユーザーをメンションまたはid] [剥奪するうんコイン]: 指定したユーザーからうんコインを剥奪するコマンド\n${process.env.PREFIX}reset [ユーザーをメンションまたはid]: 指定したユーザーのデータをリセット(初期値)にする` + '```')
             .setColor('RANDOM')
             .setFooter(`${process.env.PREFIX}help [command] で各コマンドの詳細を表示します。`, client.user.avatarURL())
             .setTimestamp(),
           new MessageEmbed()
             .setTitle(`${client.user.tag} help3ページ目(カジノコマンド)`)
-            .setDescription('```' + `${process.env.PREFIX}slot: スロットをする、借金返済中の人は強制的にマグロスロットになります\n${process.env.PREFIX}cointoss [賭け金]: コイントスをするコマンド\n${process.env.PREFIX}Littlewar [賭け金]: 小戦争\n${process.env.PREFIX}tintiro [賭け金]: チンチロ\n${process.env.PREFIX}gacha: チケットを1枚使ってガチャを引く\n${process.env.PREFIX}slotsettings [設定する名前] [設定する内容]: slotの各種設定` + '```')
+            .setDescription('```' + `${process.env.PREFIX}slot: スロットをする、借金返済中の人は強制的にマグロスロットになります\n${process.env.PREFIX}cointoss [賭け金]: コイントスをするコマンド\n${process.env.PREFIX}Littlewar [賭け金]: 小戦争\n${process.env.PREFIX}tintiro [賭け金]: チンチロ\n${process.env.PREFIX}gacha: チケットを1枚使ってガチャを引く` + '```')
             .setColor('RANDOM')
             .setFooter(`${process.env.PREFIX}help [command] で各コマンドの詳細を表示します。`, client.user.avatarURL())
             .setTimestamp()
