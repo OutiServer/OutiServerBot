146180f707f2007bce6daf34c4cbd0066c10bd84
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-03 15:17:51 +0900

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,19 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    info: {
+        name: "say",
+        description: "Botに喋らせる",
+        usage: "",
+        aliases: [""],
+        botownercommand: true,
+        botadmincommand: false
+    },
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     */
+    run: async function (client, message, args) {
+        client.channels.cache.get(args[0]).send(args[1]);
+    },
+};
