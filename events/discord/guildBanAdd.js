17954629afb15763468c34f157cd3f91acbdbcef
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-05 03:32:46 +0900

    Update guildBanAdd.js

--- events/discord/guildBanAdd.js
+++ events/discord/guildBanAdd.js
@@ -14,7 +14,7 @@ module.exports = async (client, guild, user) => {
     sql.prepare(`DELETE FROM dailys WHERE user = ${user.id} AND guild = ${guild.id}`).run();
     sql.prepare(`DELETE FROM littlewar WHERE user = ${user.id} AND guild = ${guild.id}`).run();
     const replys = ['がBANされましたwwwwwwwwwwwww', 'はシベリア旅行に行った', 'は BABANBABANBANBA BABANBABANBANBAN された', 'は下水処理場へ流れていった。', 'は豚箱に送られた', 'は粛清された', 'は北に拉致された', 'は自粛した'];
-    let random = Math.ceil(Math.random() * replys.length);
+    let random = Math.floor(Math.random() * replys.length);
     client.channels.cache.get('706459931351711775').send(user.tag + replys[random]);
     client.channels.cache.get('706452607538954263').send(`${user.tag}がゲームから追放されました <:emoji_106:790546684710223882>`);
 };
