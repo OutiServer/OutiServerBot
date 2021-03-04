a52a15fe0de25897e9238dd9699516c9f028ba91
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-04 21:33:00 +0900

    Create emoji.js

---
+++ commands/emoji.js
@@ -0,0 +1,26 @@
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+    info: {
+        name: "emoji",
+        description: "必殺絵文字人",
+        usage: "",
+        aliases: [""],
+        botownercommand: false,
+        botadmincommand: false
+    },
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     */
+    run: async function (client, message, args) {
+        if (message.member.roles.cache.has('717326376516190221')) {
+            message.member.roles.remove('717326376516190221');
+            message.reply('<:blobtukkomi:778507729517412402>');
+        }
+        else {
+            message.member.roles.add('717326376516190221');
+            message.reply('<:blobv:741467219208437800>');
+        }
+    },
+};
