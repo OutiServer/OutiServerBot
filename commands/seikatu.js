0689996986e6cb2c497b4daceba1964e2c55ec73
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-04 21:36:19 +0900

    Create seikatu.js

---
+++ commands/seikatu.js
@@ -0,0 +1,22 @@
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+    info: {
+        name: "seikatu",
+        description: "生活要素班",
+        usage: "",
+        aliases: [""],
+        botownercommand: false,
+        botadmincommand: false
+    },
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     */
+    run: async function (client, message, args) {
+        if (!message.member.roles.cache.has('814095138443100191')) {
+            message.member.roles.add('814095138443100191');
+            message.reply('<:swordthonk:798179606166634516> <#814094638255439894>');
+        }
+    },
+};
