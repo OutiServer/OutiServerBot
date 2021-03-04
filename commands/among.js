53b4ff522c53c5077af693280a6541505116d111
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-04 21:32:48 +0900

    Create among.js

---
+++ commands/among.js
@@ -0,0 +1,26 @@
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+    info: {
+        name: "among",
+        description: "among!",
+        usage: "",
+        aliases: [""],
+        botownercommand: false,
+        botadmincommand: false
+    },
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     */
+    run: async function (client, message, args) {
+        if (message.member.roles.cache.has('774593459034128395')) {
+            message.member.roles.remove('774593459034128395');
+            message.reply('<:yameru:774602751732219905>');
+        }
+        else {
+            message.member.roles.add('774593459034128395');
+            message.reply('<:yaruyo:774598967459446784>');
+        }
+    },
+};
