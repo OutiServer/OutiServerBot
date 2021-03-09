0319ecd204c24841914b0969a8071f35bb6460fb
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-09 17:02:19 +0900

    Update 0 0 15,23,7 * * *.js

--- events/cron/0 0 15,23,7 * * *.js
+++ events/cron/0 0 15,23,7 * * *.js
@@ -17,9 +17,9 @@ module.exports = (client) => {
             sql.prepare(`DELETE FROM snss WHERE user = ${data.user} AND guild = ${data.guild}`).run();
         }
     }
-    client.channels.cache.get('706452607538954263').send(`これは宣伝です！`,
+    client.channels.cache.get('706452607538954263').send(
         new MessageEmbed()
-            .setDescription(`匿名で参加できるアンケートを設置しています。暇なときに記入してみてください。貴重な意見を待っています。\n[うんこ鯖アンケート](https://forms.gle/aRtBT1piAofz3vJM6)\n[みんなが遊びたい理想の鯖とは](https://docs.google.com/forms/d/156rdFiJkwUzNsHvx9KBNnEdoTFvJINsABn7x6hP8vzw/edit)\n${content}`)
+            .setDescription(`匿名で参加できるアンケートを設置しています。暇なときに記入してみてください。貴重な意見を待っています。\n[うんこ鯖アンケート](https://forms.gle/aRtBT1piAofz3vJM6)\n[みんなが遊びたい理想の鯖とは](https://docs.google.com/forms/d/156rdFiJkwUzNsHvx9KBNnEdoTFvJINsABn7x6hP8vzw/edit)\n[うんこ鯖のためのアンケート](https://docs.google.com/forms/d/e/1FAIpQLScJNd7ka66_69_irUeDr6NPAf5t4z_oNOKFWs60UBctGWkePA/viewform?usp=sf_link)\n${content}`)
             .setColor('RANDOM')
             .setTimestamp()
     );;
