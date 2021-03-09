aff07a584e43e789cb5c6a26537a90670f95a556
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-09 22:51:41 +0900

    Update cointoss.js

--- commands/cointoss.js
+++ commands/cointoss.js
@@ -15,10 +15,9 @@ module.exports = {
 
     /**
      * @param {Message} message
-     * @param {Client} client
      */
 
-    run: async function (message, args) {
+    run: async function (client, message, args) {
         if (message.channel.id !== '798157114555105330' && message.channel.id !== '798176065562476604' && message.channel.id !== '798198069849227294' && message.channel.id !== '798486503255834664' && message.channel.id !== '798570749136601158' && message.guild.id === '706452606918066237') {
             message.react('793460058250805259');
             return message.reply('そのコマンドは<#798157114555105330>・<#798176065562476604>、<#798198069849227294>、<#798486503255834664>、<#798570749136601158>でしか使用できません<a:owoxgif:793460058250805259>');
