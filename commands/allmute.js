b53d5dc4daf279d1fb22c86125dc36aa0574bf68
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-09 21:34:13 +0900

    Update allmute.js

--- commands/allmute.js
+++ commands/allmute.js
@@ -19,6 +19,24 @@ module.exports = {
             return message.reply("このコマンドを使用するには、ボイスチャンネルに参加する必要があります！");
         }
         message.member.voice.guild.voiceStates.cache.map(member => member.setMute(true, 'Among Us!'));
-        message.reply('全員のミュートが終了しました！\n解除するには `?allunmute` を送信してください！');
+        const membermute = message.member.voice.guild.voiceStates.cache.map(member => member.serverMute);
+        let flag = false;
+        for (const data of membermute) {
+            if (data) {
+                flag = true;
+                break;
+            }
+            else {
+                flag = false;
+            }
+        }
+        if (flag) {
+            message.member.voice.guild.voiceStates.cache.map(member => member.setMute(false, 'Among Us!'));
+            message.reply('全員のミュート解除が終了しました！');
+        }
+        else {
+            message.member.voice.guild.voiceStates.cache.map(member => member.setMute(true, 'Among Us!'));
+            message.reply('全員のミュート解除が終了しました！\n解除するにはもう一度同じコマンドを送信するか `?allunmute を送信してください！');
+        }
     }
 };
