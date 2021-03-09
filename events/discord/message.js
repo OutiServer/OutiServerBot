2530d44dd014d4f10fd33a6b57c7b751593fab5f
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-09 22:26:55 +0900

    Update message.js

--- events/discord/message.js
+++ events/discord/message.js
@@ -1,4 +1,6 @@
 const { Client, Message, MessageEmbed, WebhookClient } = require('discord.js');
+const { Database } = require('../../unko/index');
+const db = new Database('unkoserver.db');
 
 /**
  * @param {Client} client
@@ -42,27 +44,10 @@ module.exports = async (client, message) => {
     });
   };
 
-  let usermoneydata = client.getMoney.get(message.author.id, message.guild.id);
-  if (!usermoneydata) {
-    usermoneydata = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, money: 0, dailylogin: 0, ticket: 0 }
-  }
+  let usermoneydata = db.MoneyGet(message.author.id, message.guild.id);
+  let userdailydata = db.DailyGet(message.author.id, message.guild.id);
 
-  let userdebtdata = client.getDebt.get(message.author.id, message.guild.id);
-  if (!userdebtdata) {
-    userdebtdata = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, Tuna: 0, Shoulder: null }
-  }
-
-  let userdailydata = client.getDaily.get(message.author.id, message.guild.id);
-  if (!userdailydata) {
-    userdailydata = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, login: 0 }
-  }
-
-  let servermoneydata = client.getServerMoney.get(message.guild.id);
-  if (!servermoneydata) {
-    servermoneydata = { id: `${message.guild.id}`, guild: message.guild.id, money: 0 }
-  }
-
-  if (userdailydata.login === 0 && userdebtdata.Tuna === 0 && message.guild.id === '706452606918066237') {
+  if (userdailydata.login === 0 && usermoneydata.tuna === 0 && message.guild.id === '706452606918066237') {
     userdailydata.login = 1;
     usermoneydata.dailylogin += 1;
     usermoneydata.money += 10000 * usermoneydata.dailylogin;
@@ -74,41 +59,37 @@ module.exports = async (client, message) => {
     );
   }
 
-  servermoneydata.money += message.content.length;
-
-  if (usermoneydata.money < 10000 && userdebtdata.Tuna === 0) {
+  if (usermoneydata.money < 10000 && usermoneydata.tuna === 0) {
     usermoneydata.money += message.content.length * 10;
     if (usermoneydata.money > 9999) {
       usermoneydata.money = 10000;
     }
   }
 
-  if (usermoneydata.money < -99999 && userdebtdata.Tuna === 0) {
+  if (usermoneydata.money < -99999 && usermoneydata.tuna === 0) {
     message.member.roles.add('798570033235755029');
-    userdebtdata.Tuna = 1;
+    usermoneydata.tuna = 1;
     const webhook = new WebhookClient('798186603235246111', 'Rf6vyUbm7GuwLOmmHseu-QZp7bV7QOYykwEoqzrSLX3Rjkza_7ipOsbJQGe9BKoGNiHn');
     webhook.send(`${message.author}、開けろごらああ！てめえ自分が何シてんのかわかってるのか！！？\n${usermoneydata.money * -1}円、しっかり払ってもらうで`)
   }
-  else if (usermoneydata.money > -1 && userdebtdata.Tuna === 1) {
+  else if (usermoneydata.money > -1 && usermoneydata.tuna === 1) {
+    usermoneydata.tuna = 0;
     message.member.roles.remove('798570033235755029');
     const webhook = new WebhookClient('798186603235246111', 'Rf6vyUbm7GuwLOmmHseu-QZp7bV7QOYykwEoqzrSLX3Rjkza_7ipOsbJQGe9BKoGNiHn');
     webhook.send(`${message.author}、確かに借金は返してもらった、もう二度と借金すんじゃねえぞ。`);
-    userdebtdata.Tuna = 0;
   }
 
   if (usermoneydata.ticket === null) {
     usermoneydata.ticket = 0;
   }
 
-  client.setMoney.run(usermoneydata);
-  client.setDebt.run(userdebtdata);
-  client.setDaily.run(userdailydata);
-  client.setServerMoney.run(servermoneydata);
+  db.MoneySet(usermoneydata);
+  db.DailySet(userdailydata);
 
-  if (message.channel.id === '798157114555105330' || message.channel.id === '798176065562476604' || message.channel.id === '798198069849227294' || message.channel.id === '798570749136601158' || message.channel.id === '798486503255834664' || message.channel.id === '798186469516116028' || message.channel.id === '798571746730049597' || message.channel.id === '798191278369931265' || message.channel.id === '798500844579586048') {
+  if (message.channel.parentID === '798186356105936956') {
     message.member.roles.add('798533393448960020');
   }
-  else if (message.channel.id === '738899882454024323' || message.channel.id === '801057145529172018' || message.channel.id === '801095344800661544') {
+  else if (message.channel.parentID === '801057223139917884') {
     message.member.roles.add('801796340057112589');
   }
 
