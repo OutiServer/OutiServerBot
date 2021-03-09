bd14dc420a3b0469a59f92925b5991f898bc0a24
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-09 22:15:24 +0900

    Update database.js

--- unko/database.js
+++ unko/database.js
@@ -9,30 +9,42 @@ class Database {
     }
 
     Initialize() { //初期設定色々
-        this.sql.prepare('DROP TABLE enemys;').run();
-        this.sql.prepare('DROP TABLE enemyspells;').run();
-        this.sql.prepare('DROP TABLE players;').run();
-        //テーブル作成とか
-        const Enemytable = this.sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'enemys';").get();
-        if (!Enemytable['count(*)']) {
-            this.sql.prepare("CREATE TABLE enemys (id TEXT PRIMARY KEY, user TEXT, number INTEGER, flag INTEGER, hp INTEGER, spellcardnumber INTEGER, spellcardflag INTEGER, maxspellcard INTEGER, maxatk INTEGER, spellcardtype INTEGER, spellbonus INTEGER);").run();
-            this.sql.prepare("CREATE UNIQUE INDEX idx_enemys_id ON enemys (id);").run();
+        const Moneytable = this.sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'moneys';").get();
+        if (!Moneytable['count(*)']) {
+            this.sql.prepare("CREATE TABLE moneys (id TEXT PRIMARY KEY, user TEXT, guild TEXT, money INTEGER, dailylogin INTEGER, ticket INTEGER, tuna INTEGER);").run();
+            this.sql.prepare("CREATE UNIQUE INDEX idx_moneys_id ON moneys (id);").run();
             this.sql.pragma("synchronous = 1");
             this.sql.pragma("journal_mode = wal");
         }
 
-        const EnemySpelltable = this.sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'enemyspells';").get();
-        if (!EnemySpelltable['count(*)']) {
-            this.sql.prepare("CREATE TABLE enemyspells (id TEXT PRIMARY KEY, user TEXT, enemynumber INTEGER, spellnumber INTEGER, allspell INTEGER, getspell INTEGER);").run();
-            this.sql.prepare("CREATE UNIQUE INDEX idx_enemyspells_id ON enemyspells (id);").run();
+        const SlotSettingstable = this.sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'slotsettings';").get();
+        if (!SlotSettingstable['count(*)']) {
+            this.sql.prepare("CREATE TABLE slotsettings (id TEXT PRIMARY KEY, guild TEXT, Jackpotprobability INTEGER, Jackpot INTEGER);").run();
+            this.sql.prepare("CREATE UNIQUE INDEX idx_slotsettings_id ON slotsettings (id);").run();
             this.sql.pragma("synchronous = 1");
             this.sql.pragma("journal_mode = wal");
         }
 
-        const Playertable = this.sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'players';").get();
-        if (!Playertable['count(*)']) {
-            this.sql.prepare("CREATE TABLE players (id TEXT PRIMARY KEY, user TEXT, number INTEGER, hiscore INTEGER, score INTEGER, hp INTEGER, power INTEGER, player INTEGER, spell INTEGER, playerfragment INTEGER, spellfragment INTEGER);").run();
-            this.sql.prepare("CREATE UNIQUE INDEX idx_players_id ON players (id);").run();
+        const Dailytable = this.sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'dailys';").get();
+        if (!Dailytable['count(*)']) {
+            this.sql.prepare("CREATE TABLE dailys (id TEXT PRIMARY KEY, user TEXT, guild TEXT, login INTEGER);").run();
+            this.sql.prepare("CREATE UNIQUE INDEX idx_dailys_id ON dailys (id);").run();
+            this.sql.pragma("synchronous = 1");
+            this.sql.pragma("journal_mode = wal");
+        }
+
+        const Littlewartable = this.sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'littlewar';").get();
+        if (!Littlewartable['count(*)']) {
+            this.sql.prepare("CREATE TABLE littlewar (id TEXT PRIMARY KEY, user TEXT, guild TEXT, emoji1 INTEGER, emoji2 INTEGER, emoji3 INTEGER, number INTEGER);").run();
+            this.sql.prepare("CREATE UNIQUE INDEX idx_littlewar_id ON littlewar (id);").run();
+            this.sql.pragma("synchronous = 1");
+            this.sql.pragma("journal_mode = wal");
+        }
+
+        const Snstable = this.sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'snss';").get();
+        if (!Snstable['count(*)']) {
+            this.sql.prepare("CREATE TABLE snss (id TEXT PRIMARY KEY, user TEXT, guild TEXT, title TEXT, url TEXT, count INTEGER);").run();
+            this.sql.prepare("CREATE UNIQUE INDEX idx_snss_id ON snss (id);").run();
             this.sql.pragma("synchronous = 1");
             this.sql.pragma("journal_mode = wal");
         }
@@ -40,74 +52,96 @@ class Database {
 
     /**
      * @param {string} userid
+     * @param {string} guildid
      */
 
-    EnemyGet(userid) {
-        let data = this.sql.prepare('SELECT * FROM enemys WHERE user = ?').get(userid);
+    MoneyGet(userid, guildid) {
+        let data = this.sql.prepare('SELECT * FROM moneys WHERE user = ? AND guild = ?').get(userid, guildid);
         if (!data) {
-            data = { id: `${userid}`, user: userid, number: 0, flag: 0, hp: 0, spellcardnumber: -1, spellcardflag: 0, maxspellcard: 0, maxatk: 0, spellcardtype: 0, spellbonus: 0 }
-            this.EnemySet(data);
+            data = { id: `${guildid}-${userid}`, user: userid, guild: guildid, money: 0, dailylogin: 0, ticket: 0, tuna: 0 }
+            this.MoneySet(data);
         }
 
         return data;
     }
 
-    EnemySet(data) {
-        this.sql.prepare('INSERT OR REPLACE INTO enemys (id, user, number, flag, hp, spellcardnumber, spellcardflag, maxspellcard, maxatk, spellcardtype, spellbonus) VALUES (@id, @user, @number, @flag, @hp, @spellcardnumber, @spellcardflag, @maxspellcard, @maxatk, @spellcardtype, @spellbonus);').run(data);
+    MoneySet(data) {
+        this.sql.prepare('INSERT OR REPLACE INTO moneys (id, user, guild, money, dailylogin, ticket, tuna) VALUES (@id, @user, @guild, @money, @dailylogin, @ticket, @tuna);').run(data);
+    }
+
+    /**
+     * @param {string} guildid
+     */
+
+    SlotSettingsGet(guildid) {
+        let data = this.sql.prepare('SELECT * FROM slotsettings WHERE guild = ?').get(guildid);
+        if (!data) {
+            data = { id: `${guildid}`, guild: guildid, Jackpotprobability: 10, Jackpot: 100000 }
+            this.SlotSettingsSet(data);
+        }
+
+        return data;
+    }
+
+    SlotSettingsSet(data) {
+        this.sql.prepare('INSERT OR REPLACE INTO slotsettings (id, guild, Jackpotprobability, Jackpot) VALUES (@id, @guild, @Jackpotprobability, @Jackpot);').run(data);
     }
 
     /**
      * @param {string} userid
-     * @param {number} enemynumber
-     * @param {number} spellnumber
+     * @param {string} guildid
      */
 
-    EnemySpellGet(userid, enemynumber, spellnumber) {
-        let data = this.sql.prepare('SELECT * FROM enemyspells WHERE user = ? AND enemynumber = ? AND spellnumber = ?').get(userid, enemynumber, spellnumber);
+    DailyGet(userid, guildid) {
+        let data = this.sql.prepare('SELECT * FROM dailys WHERE user = ? AND guild = ?').get(userid, guildid);
         if (!data) {
-            data = { id: `${userid}-${enemynumber}-${spellnumber}`, user: userid, enemynumber: enemynumber, spellnumber: spellnumber, allspell: 0, getspell: 0 };
-            this.EnemySpellSet(data);
+            data = { id: `${userid}-${guildid}`, user: userid, guild: guildid, login: 0 }
+            this.DailySet(data);
         }
 
         return data;
     }
 
-    EnemySpellSet(data) {
-        this.sql.prepare('INSERT OR REPLACE INTO enemyspells(id, user, enemynumber, spellnumber, allspell, getspell) VALUES(@id, @user, @enemynumber, @spellnumber, @allspell, @getspell);').run(data);
+    DailySet(data) {
+        this.sql.prepare('INSERT OR REPLACE INTO dailys (id, user, guild, login) VALUES (@id, @user, @guild, @login);').run(data);
     }
 
     /**
-    * @param {string} userid
-    */
+     * @param {string} userid
+     * @param {string} guildid
+     */
 
-    PlayerGet(userid) {
-        let data = this.sql.prepare('SELECT * FROM players WHERE user = ?').get(userid);
+    LittlewarGet(userid, guildid) {
+        let data = this.sql.prepare('SELECT * FROM littlewar WHERE guild = ?').get(guildid);
         if (!data) {
-            data = { id: `${userid}`, user: userid, number: 0, hiscore: 0, score: 0, hp: 50000, power: 128, player: 3, spell: 3, playerfragment: 0, spellfragment: 0 };
-            this.PlayerSet(data);
+            data = { id: guildid, user: userid, guild: guildid, emoji1: 1, emoji2: 1, emoji3: 1, number: 0 }
+            this.LittlewarSet(data);
         }
 
         return data;
     }
 
-    PlayerSet(data) {
-        this.sql.prepare('INSERT OR REPLACE INTO players (id, user, number, hiscore, score, hp, power, player, spell, playerfragment, spellfragment) VALUES (@id, @user, @number, @hiscore, @score, @hp, @power, @player, @spell, @playerfragment, @spellfragment);').run(data);
+    LittlewarSet(data) {
+        this.sql.prepare('INSERT OR REPLACE INTO littlewar (id, user, guild, emoji1, emoji2, emoji3, number) VALUES (@id, @user, @guild, @emoji1, @emoji2, @emoji3, @number);').run(data);
     }
 
     /**
-     * @param {string} sql_source
+     * @param {string} userid
+     * @param {string} guildid
      */
 
-    DataGet(sql_source) { //データ取得関数
-        return this.sql.prepare(sql_source).get();
+    SnsGet(userid, guildid) {
+        let data = this.sql.prepare('SELECT * FROM snss WHERE user = ? AND guild = ?').get(userid, guildid);
+        if (!data) {
+            data = { id: `${guildid}-${userid}`, user: userid, guild: guildid, title: '', url: '', count: 0 }
+            this.SnsSet(data);
+        }
+
+        return data;
     }
 
-    /**
-     * @param {string} sql_source
-     */
-
-    DataSet(sql_source, data) { //データセット関数
-        this.sql.prepare(sql_source).run(data);
+    SnsSet(data) {
+        this.sql.prepare('INSERT OR REPLACE INTO snss (id, user, guild, title, url, count) VALUES (@id, @user, @guild, @title, @url, @count);').run(data);
     }
 }
 
