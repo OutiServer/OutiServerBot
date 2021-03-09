78c2b67ec46168c4c1a7c9cd0aeda4bc02f6d7bf
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-09 22:32:41 +0900

    Update 0 0 15 * * *.js

--- events/cron/0 0 15 * * *.js
+++ events/cron/0 0 15 * * *.js
@@ -1,4 +1,4 @@
-const { Client, MessageAttachment } = require('discord.js');
+const { Client } = require('discord.js');
 const SQLite = require("better-sqlite3");
 const sql = new SQLite('unkoserver.db');
 const { Database } = require('../../unko/index');
@@ -9,25 +9,14 @@ const db = new Database('unkoserver.db');
  */
 
 module.exports = (client) => {
-    const time = new Date();
     db.backup(client);
-    sql.prepare("DROP TABLE dailys;").run();
-    const testtable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'dailys';").get();
-    if (!testtable['count(*)']) {
-        sql.prepare("CREATE TABLE dailys (id TEXT PRIMARY KEY, user TEXT, guild TEXT, login INTEGER);").run();
-        sql.prepare("CREATE UNIQUE INDEX idx_dailys_id ON dailys (id);").run();
-        sql.pragma("synchronous = 1");
-        sql.pragma("journal_mode = wal");
-    }
-    let servermoneydata = client.getServerMoney.get('706452606918066237');
+    db.dailyreset();
     const all = sql.prepare("SELECT * FROM moneys WHERE guild = ? ORDER BY money DESC;").all('706452606918066237');
     for (let data of all) {
         if (data.money > 1) {
             const zeikin = Math.ceil(data.money / 1.15);
             data.money -= zeikin;
-            servermoneydata.money += zeikin;
-            client.setMoney.run(data);
+            db.MoneySet(data);
         }
     }
-    client.setServerMoney.run(servermoneydata);
 };
