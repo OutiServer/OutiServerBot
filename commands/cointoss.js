6b2c95519851a11dd7ec623b4397f2d5fc947d09
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-09 22:39:47 +0900

    Update cointoss.js

--- commands/cointoss.js
+++ commands/cointoss.js
@@ -1,5 +1,7 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
 const { Menu } = require('discord.js-menu');
+const { Database } = require('../unko/index');
+const db = new Database('unkoserver.db');
 
 module.exports = {
     info: {
@@ -10,32 +12,30 @@ module.exports = {
         botownercommand: false,
         botadmincommand: false
     },
+
     /**
      * @param {Message} message
      * @param {Client} client
      */
+
     run: async function (client, message, args) {
         if (message.channel.id !== '798157114555105330' && message.channel.id !== '798176065562476604' && message.channel.id !== '798198069849227294' && message.channel.id !== '798486503255834664' && message.channel.id !== '798570749136601158' && message.guild.id === '706452606918066237') {
             message.react('793460058250805259');
             return message.reply('そのコマンドは<#798157114555105330>・<#798176065562476604>、<#798198069849227294>、<#798486503255834664>、<#798570749136601158>でしか使用できません<a:owoxgif:793460058250805259>');
         }
-        let userdebtdata = client.getDebt.get(message.author.id, message.guild.id);
-        if (!userdebtdata) {
-            userdebtdata = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, Tuna: 0, Shoulder: null }
-        }
-        if (userdebtdata.Tuna === 1) {
+
+        let usermoneydata = db.MoneyGet(message.author.id, message.guild.id);
+        if (usermoneydata.tuna === 1) {
             message.react('793460058250805259');
             return message.reply('借金返済中にコイントスはできません');
         }
+
         const Stake = Number(args[0]);
         if (!Stake || Stake < 50000 || Stake > 100000) {
             message.react('793460058250805259');
             return message.reply('第一引数に賭け金を50000から100000の数値で入れてください！');
         }
-        let usermoneydata = client.getMoney.get(message.author.id, message.guild.id);
-        if (!usermoneydata) {
-            usermoneydata = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, money: 0, dailylogin: 0, ticket: 0 }
-        }
+
         let cointossMenu = new Menu(message.channel, message.author.id, [
             {
                 name: 'main',
@@ -100,7 +100,7 @@ module.exports = {
                 }
             }
             message.channel.send(embed);
-            client.setMoney.run(usermoneydata);
+            db.MoneySet(usermoneydata);
         })
     },
 };
