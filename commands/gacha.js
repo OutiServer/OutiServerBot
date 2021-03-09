b0347668a7fa96ed2a00f5a7c262397e6c9af70e
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-09 22:40:29 +0900

    Update gacha.js

--- commands/gacha.js
+++ commands/gacha.js
@@ -11,10 +11,12 @@ module.exports = {
         botownercommand: false,
         botadmincommand: false
     },
+
     /**
      * @param {Message} message
      * @param {Client} client
      */
+
     run: async function (client, message, args) {
         if (message.channel.id !== '798157114555105330' && message.channel.id !== '798176065562476604' && message.channel.id !== '798198069849227294' && message.channel.id !== '798486503255834664' && message.channel.id !== '798570749136601158' && message.guild.id === '706452606918066237') {
             message.react('793460058250805259');
