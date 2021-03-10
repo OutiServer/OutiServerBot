07ac6fdd4ffb9cfa2a3da8d8f13abf5a43c321b4
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-10 10:32:38 +0900

    Update voiceStateUpdate.js

--- events/discord/voiceStateUpdate.js
+++ events/discord/voiceStateUpdate.js
@@ -7,6 +7,10 @@ const { Client, GuildMember } = require('discord.js');
  */
 
 module.exports = async (client, oldMember, newMember) => {
+  const conn = client.voice.connections.get(process.env.DISCORD_GUILD_ID);
+  if (conn && conn.channel && conn.channel.members.array().length < 2) {
+    conn.disconnect();
+  }
   if (oldMember.guild.id !== '706452606918066237' || oldMember.member.user.bot) return;
   if (oldMember.channelID === null) {
     client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
