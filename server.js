dc1eab575dd18ea37589ff5c9d93c05cb24716d4
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-10 21:55:53 +0900

    web

---
+++ server.js
@@ -0,0 +1,30 @@
+const { Client } = require('discord.js');
+const express = require("express");
+const path = require('path');
+const util = require('minecraft-server-util');
+const app = express();
+
+/**
+ * @param {Client} client
+ */
+
+module.exports = {
+    run: async function (client) {
+        app.use(express.static(path.join(__dirname, 'public')));
+        app.set("view engine", "ejs");
+
+        app.listen(process.env.PORT, () => {
+            console.log(`サーバーを起動させました！`);
+        });
+
+        app.get("/", (req, res) => {
+            util.statusBedrock('126.235.33.140', { timeout: 5000 })
+                .then((result) => {
+                    res.render(__dirname + '/public/html/index.ejs', { joindedmembers: `${result.onlinePlayers}.${result.maxPlayers}`, serverversion: result.version });
+                })
+                .catch(() => {
+                    res.sendFile(__dirname + '/public/html/index.index.js');
+                })
+        });
+    }
+};
