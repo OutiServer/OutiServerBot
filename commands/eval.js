495df59808a8747ade64089314979388f2c3c55f
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-11 15:32:55 +0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,6 @@
 const { Message, MessageEmbed } = require('discord.js');
 const { Menu } = require('discord.js-menu');
+const { json } = require('express');
 const { inspect } = require('util');
 
 module.exports = {
@@ -17,56 +18,15 @@ module.exports = {
    */
 
   run: async function (client, message, args) {
-    let evalMenu = new Menu(message.channel, message.author.id, [
-      {
-        name: 'main',
-        content: new MessageEmbed()
-          .setDescription('```js\n' + args.join(' ') + '```')
-          .setColor('RANDOM')
-          .setTimestamp(),
-        reactions: {
-          '793460057932038145': 'yes',
-          '793460058250805259': 'delete'
-        }
-      },
-      {
-        name: 'yes',
-        content: new MessageEmbed()
-          .setDescription('```js\n' + args.join(' ') + '```')
-          .setColor('RANDOM')
-          .setTimestamp(),
-        reactions: {
-          '⏹': 'delete'
-        }
-      }
-    ], 60000)
-    evalMenu.start()
-    evalMenu.on('pageChange', async destination => {
-      if (destination.name === "yes") {
-        let evaled;
-        try {
-          evaled = await eval(args.join(' '));
-          const evalinsoext = inspect(evaled).length
-          if (evalinsoext <= 2000) {
-            message.channel.send(new MessageEmbed()
-              .setDescription('```' + inspect(evaled) + '```')
-              .setColor('RANDOM')
-              .setTimestamp());
-          }
-          else {
-            message.reply(new MessageEmbed()
-              .setDescription('```実行結果が2000文字を超過しているため表示できません```')
-              .setColor('RANDOM')
-              .setTimestamp());
-          }
-        }
-        catch (error) {
-          message.channel.send(new MessageEmbed()
-            .setDescription('```' + error + '```')
-            .setColor('RANDOM')
-            .setTimestamp());
-        }
-      }
-    })
+    let evaled;
+    try {
+      evaled = await eval(args.join(' '));
+      message.channel.send(inspect(evaled), { code: true, split: true });
+      console.log(inspect(evaled));
+    }
+    catch (error) {
+      console.error(error);
+      message.reply(error, { code: true });
+    }
   },
 };
