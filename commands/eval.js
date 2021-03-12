740543b0a1fe869124e79bc0174a50e0ee25427a
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-12 15:45:48 +0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -18,15 +18,54 @@ module.exports = {
    */
 
   run: async function (client, message, args) {
-    let evaled;
-    try {
-      evaled = await eval(args.join(' '));
-      message.channel.send(inspect(evaled), { code: true, split: true });
-      console.log(inspect(evaled));
-    }
-    catch (error) {
-      console.error(error);
-      message.reply(error, { code: true });
-    }
+    let evalMenu = new Menu(message.channel, message.author.id, [
+      {
+        name: 'main',
+        content: new MessageEmbed()
+          .setDescription('```js\n' + args.join(' ') + '```')
+          .setColor('RANDOM')
+          .setTimestamp(),
+        reactions: {
+          '810436146718441483': 'yes',
+          '810436146978619392': 'delete'
+        }
+      },
+      {
+        name: 'yes',
+        content: new MessageEmbed()
+          .setDescription('```js\n' + args.join(' ') + '```')
+          .setColor('RANDOM')
+          .setTimestamp(),
+        reactions: {
+          '⏹': 'delete'
+        }
+      }
+    ], 60000)
+    evalMenu.start()
+    evalMenu.on('pageChange', async destination => {
+      if (destination.name === "yes") {
+        let evaled;
+        try {
+          evaled = await eval(args.join(' '));
+          const evalinsoext = inspect(evaled).length
+          if (evalinsoext <= 2000) {
+            message.react('✅');
+            message.channel.send(new MessageEmbed()
+              .setDescription('```' + inspect(evaled) + '```')
+              .setColor('RANDOM')
+              .setTimestamp());
+          }
+          else {
+            message.react('❌');
+          }
+        }
+        catch (error) {
+          message.reply(new MessageEmbed()
+            .setDescription('```' + error + '```')
+            .setColor('RANDOM')
+            .setTimestamp());
+        }
+      }
+    })
   },
 };
