1b1fb38e344ec01c0824f82695a68c485a04d881
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-16 15:59:43 +0900

    Create restart.js

---
+++ commands/restart.js
@@ -0,0 +1,25 @@
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+    info: {
+        name: "restart",
+        description: "再接続",
+        usage: "",
+        aliases: [""],
+        owneronly: true,
+        adminonly: false,
+        category: 'Owner'
+    },
+
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     */
+
+    run: async function (client, message, args) {
+        message.channel.send('再接続しています...')
+            .then(() => client.destroy())
+            .then(() => client.login(process.env.DISCORD_TOKEN))
+            .then(() => message.channel.send('再接続が完了しました'));
+    },
+};
