6c302daf14e1315054c9156f8bc392fdb1ba29e0
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-17 21:08:52 +0900

    Update guildMemberAdd.js

--- events/discord/guildMemberAdd.js
+++ events/discord/guildMemberAdd.js
@@ -7,5 +7,6 @@ const { Client, GuildMember } = require('discord.js');
 
 module.exports = async (client, member) => {
     if (member.user.bot === true || member.guild.id !== '706452606918066237') return;
+    member.roles.add('821715178147020800');
     client.channels.cache.get('797008715646500865').send(`${member}さん、よろしくお願いします！\n` + 'うんこサーバーへようこそ！\n<#794787771100954635>を確認して\n<#780211443201998888>からサーバーに入ってください！\nカジノランドとvcチャンネルはミュート推奨です！');
 };
