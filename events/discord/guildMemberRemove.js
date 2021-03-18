b241c6920e733a0240f99fc7ae3314d88b254afc
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-18 18:51:27 +0900

    Update guildMemberRemove.js

--- events/discord/guildMemberRemove.js
+++ events/discord/guildMemberRemove.js
@@ -7,6 +7,7 @@ const { Client, GuildMember } = require('discord.js');
 
 
 module.exports = async (client, member) => {
-    if(member.user.bot === true || member.guild.id !== '706452606918066237') return;
+    if (member.user.bot === true || member.guild.id !== '706452606918066237') return;
     client.channels.cache.get('706459931351711775').send(`${member.user.tag} マサラタウンにさよならばいばい`);
+    member.send('うんこ鯖の利用アンケートにご協力ください\nhttps://docs.google.com/forms/d/166hmvv-eYtmazsENRWvl9i5JrCiDe5TnxRnyEB5VhmA/edit');
 };
