1c59fc9d59e6413418b9052dd1f787c20acffc63
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-20 08:39:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -48,14 +48,4 @@ fs.readdir(__dirname + "/events/cron/", (err, files) => {
   });
 });
 
-const disboarddata = db.DisboardtimerGet('706452606918066237');
-cron.schedule(`${disboarddata.second} ${disboarddata.minute} ${disboarddata.hour} * * *`, () => {
-  client.channels.cache.get('706452606918066237').send('Bumpしてから二時間経ちました\n`!d bump` を実行しましょう<:emoji_121:820198227147751474>')
-});
-
-const dissokudata = db.DissokutimerGet('706452606918066237');
-cron.schedule(`${dissokudata.second} ${dissokudata.minute} ${dissokudata.hour} * * *`, () => {
-  client.channels.cache.get('706452606918066237').send('Upしてから一時間経ちました\n`/dissoku up!` を実行しましょう<:emoji_121:820198227147751474>')
-});
-
 client.login();
