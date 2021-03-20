a64eadc7d8251262ef88ed0ac0a2eaa5ca904d4a
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-20 20:54:18 +0900

    Update blackjack.js

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -4,6 +4,7 @@ const db = new Database('unkoserver.db');
 const cards = require('../dat/cards.json');
 const dealer = new Map();
 const player = new Map();
+const winloseflag = new Map();
 
 module.exports = {
     info: {
@@ -21,18 +22,6 @@ module.exports = {
      */
 
     run: async function (client, message, args) {
-        if (!dealer.get(message.author.id)) {
-            const dealerarray = [];
-            const playerarray = [];
-            for (let i = 0; i < 2; i++) {
-                dealerarray.push(cards[0][Math.floor(Math.random() * cards[0].length)]);
-            }
-            for (let i = 0; i < 2; i++) {
-                playerarray.push(cards[0][Math.floor(Math.random() * cards[0].length)]);
-            }
-            dealer.set(message.author.id, dealerarray);
-            player.set(message.author.id, playerarray);
-        }
         if (message.channel.id !== '798157114555105330' && message.channel.id !== '798176065562476604' && message.channel.id !== '798198069849227294' && message.channel.id !== '798486503255834664' && message.channel.id !== '798570749136601158' && message.guild.id === '706452606918066237') {
             message.react('793460058250805259');
             return message.reply('そのコマンドは<#798157114555105330>・<#798176065562476604>、<#798198069849227294>、<#798486503255834664>、<#798570749136601158>でしか使用できません<a:owoxgif:793460058250805259>');
@@ -44,15 +33,85 @@ module.exports = {
             return message.reply('借金返済中にブラックジャックはできません');
         }
 
+        if (!dealer.get(message.author.id)) {
+            const dealerarray = [];
+            const playerarray = [];
+            for (let i = 0; i < 2; i++) {
+                dealerarray.push(Math.floor(Math.random() * cards[0].length));
+            }
+            for (let i = 0; i < 2; i++) {
+                playerarray.push(Math.floor(Math.random() * cards[0].length));
+            }
+            dealer.set(message.author.id, dealerarray);
+            player.set(message.author.id, playerarray);
+        }
 
         const msg = await message.channel.send(
             new MessageEmbed()
                 .setTitle('BlackJack')
-                .addField('ディーラー', dealer.get(message.author.id).join(' '))
-                .addField('あなた', player.get(message.author.id).join(' '))
+                .setDescription('操作を送信してください\n`Stand`')
+                .addField('ディーラー', dealer.get(message.author.id).map(data => cards[0][data]).join(' '))
+                .addField('あなた', player.get(message.author.id).map(data => cards[0][data]).join(' '))
                 .setColor('RANDOM')
                 .setTimestamp()
         );
+
+        while (true) {
+            const filter = msg => msg.author.id === message.author.id;
+            const collected = await message.channel.awaitMessages(filter, { max: 1, time: 60000 });
+            const response = collected.first();
+            if (!response) break;
+            if (response.content === 'Stand') break;
+        }
+
+        let dealernumber = 0;
+        let playernumber = 0;
+        const dealerarray = player.get(message.author.id);
+
+        while (winloseflag.get(message.author.id) === 1) {
+            dealerarray.push(Math.floor(Math.random() * cards[0].length));
+            for (const data of dealerarray) {
+                if (data < 10) {
+                    dealernumber += data + 1;
+                }
+                else {
+                    dealernumber += 10;
+                }
+            }
+
+            for (const data of player.get(message.author.id)) {
+                if (data < 10) {
+                    playernumber += data + 1;
+                }
+                else {
+                    playernumber += 10;
+                }
+            }
+
+            if (dealernumber > 17) {
+                if (dealernumber === 21) {
+                    winloseflag.set(message.author.id, -1);
+                    break;
+                }
+                else if (dealernumber > 21) {
+                    winloseflag.set(message.author.id, -3);
+                    break;
+                }
+                else if (dealernumber > playernumber) {
+                    winloseflag.set(message.author.id, -2);
+                    break;
+                }
+                else if (dealernumber < playernumber) {
+                    winloseflag.set(message.author.id, 3);
+                    break;
+                }
+                else {
+                    winloseflag.set(message.author.id, 10);
+                    break;
+                }
+            }
+        }
+
         dealer.delete(message.author.id);
         player.delete(message.author.id);
     }
