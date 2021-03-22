bdfe7be76e4be42a1f31ad7d31524094ba7bfbcf
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-22 23:14:59 +0900

    Create levels.js

---
+++ commands/levels.js
@@ -0,0 +1,22 @@
+const { Message } = require('discord.js');
+
+module.exports = {
+    info: {
+        name: "rank",
+        description: "MyrankとLevel確認",
+        usage: "",
+        aliases: [""],
+        owneronly: false,
+        adminonly: false,
+        category: 'Main'
+    },
+
+    /**
+     * @param {*} client 
+     * @param {Message} message 
+     */
+
+    run: async function (client, message, args) {
+
+    }
+}
