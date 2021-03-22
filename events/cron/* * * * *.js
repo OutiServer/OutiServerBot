50d44db28db47efe5f4bb5f5b904775c0a08be35
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-22 22:47:51 +0900

    Update * * * * *.js

--- events/cron/* * * * *.js
+++ events/cron/* * * * *.js
@@ -1,38 +1,11 @@
 const { Client, MessageEmbed } = require('discord.js');
 const util = require('minecraft-server-util');
-const SQLite = require("better-sqlite3");
-const sql = new SQLite('unkoserver.db');
-const { Database } = require('../../unko/index');
-const db = new Database('unkoserver.db');
 
 /**
  * @param {Client} client
  */
 
 module.exports = (client) => {
-    client.channels.cache.get('798479605764718592').messages.fetch('799635530882744372')
-        .then(msg => {
-            const Win = db.SlotSettingsGet('706452606918066237').Jackpot;
-            const embed = new MessageEmbed()
-                .setDescription(`現在のジャックポット: ${Win}うんコイン`)
-                .setColor('RANDOM')
-                .setTimestamp();
-            const top10 = sql.prepare("SELECT * FROM moneys WHERE guild = ? ORDER BY money DESC LIMIT 10;").all('706452606918066237');
-            let rank = 1;
-            for (const data of top10) {
-                const user = client.guilds.cache.get('706452606918066237').member(data.user);
-                let usertag = ''
-                if (!user) {
-                    usertag = '取得できないユーザー';
-                }
-                else {
-                    usertag = user.user.tag;
-                }
-                embed.addFields({ name: `${rank}位: ${usertag}`, value: `${data.money}うんコイン` });
-                rank++;
-            }
-            msg.edit(embed);
-        })
     client.channels.cache.get('780012050163302420').messages.fetch('800279738509426728')
         .then(msg => {
             util.statusBedrock('126.235.33.140', { timeout: 5000 })
