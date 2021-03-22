648aefec147c5461924d17e9f4ba5eb744885658
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-23 02:21:21 +0900

    Create add.js

---
+++ commands/add.js
@@ -0,0 +1,37 @@
+const { Message } = require("discord.js");
+const { Database } = require('../unko/index');
+const db = new Database('unkoserver.db');
+
+module.exports = {
+    info: {
+        name: "add",
+        description: "ユーザーに経験値追加",
+        usage: "[ユーザーをメンションまたはID] [付与する経験値]",
+        aliases: [""],
+        owneronly: false,
+        adminonly: true,
+        category: 'Admin'
+    },
+
+    /**
+     * @param {*} client 
+     * @param {Message} message 
+     */
+
+    run: async function (client, message, args) {
+        const user = message.mentions.users.first() || message.guild.member(args[0]);
+        if (user) return message.reply('経験値を付与するユーザーをメンションするかIDを第一引数に入れてください！');
+
+        const addxp = Number(args[0]);
+        if (!addxp) return message.reply('経験値を付与する数を第一引数に入れてください！');
+
+        let userleveldata = db.levelget(user.id, message.guild.id);
+
+        userleveldata.xp += addxp;
+        userleveldata.allxp += addxp;
+
+        db.levelset(userleveldata);
+
+        message.channel.send(`${user}に${addxp}経験値付与しました！`);
+    }
+}
