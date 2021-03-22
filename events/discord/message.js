921a292d059f0dbf2b4c61f5fdd95829e6e5ae89
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-22 22:50:14 +0900

    Update message.js

--- events/discord/message.js
+++ events/discord/message.js
@@ -1,4 +1,4 @@
-const { Client, Message, MessageEmbed, WebhookClient } = require('discord.js');
+const { Client, Message, MessageEmbed } = require('discord.js');
 const textToSpeech = require('@google-cloud/text-to-speech');
 const { Readable } = require('stream');
 const { Database } = require('../../unko/index');
@@ -41,12 +41,11 @@ module.exports = async (client, message) => {
 
   if (message.channel.name === 'うんこ鯖グローバルチャット' || message.channel.name === 'カスクラグローバルチャット') {
     if (message.attachments.size <= 0) {
-      message.delete()
+      message.delete();
     }
     client.channels.cache.forEach(channel => {
       let username = message.author.tag;
-      if (message.member.nickname)
-        username = message.member.nickname + `(${message.author.tag})`;
+      if (message.member.nickname) username = message.member.nickname + `(${message.author.tag})`;
       if (message.attachments.size <= 0) {
         const embed = new MessageEmbed()
           .setAuthor(username, message.author.avatarURL())
@@ -88,45 +87,10 @@ module.exports = async (client, message) => {
     usermoneydata.tuna = 0;
   }
 
-  if (userdailydata.login === 0 && usermoneydata.tuna === 0 && message.guild.id === '706452606918066237') {
-    userdailydata.login = 1;
-    usermoneydata.dailylogin += 1;
-    usermoneydata.money += 10000 * usermoneydata.dailylogin;
-    message.channel.send(
-      new MessageEmbed()
-        .setDescription(`<@${message.author.id}>、あなたは現在うんこ鯖に${usermoneydata.dailylogin}日浮上しています！\nデイリーボーナスとして${10000 * usermoneydata.dailylogin}うんコイン獲得しました！`)
-        .setColor('RANDOM')
-        .setTimestamp()
-    );
-  }
-
-  if (usermoneydata.money < 10000 && usermoneydata.tuna === 0) {
-    usermoneydata.money += message.content.length * 10;
-    if (usermoneydata.money > 9999) {
-      usermoneydata.money = 10000;
-    }
-  }
-
-  if (usermoneydata.money < -99999 && usermoneydata.tuna === 0) {
-    message.member.roles.add('798570033235755029');
-    usermoneydata.tuna = 1;
-    const webhook = new WebhookClient('798186603235246111', 'Rf6vyUbm7GuwLOmmHseu-QZp7bV7QOYykwEoqzrSLX3Rjkza_7ipOsbJQGe9BKoGNiHn');
-    webhook.send(`${message.author}、開けろごらああ！てめえ自分が何シてんのかわかってるのか！！？\n${usermoneydata.money * -1}円、しっかり払ってもらうで`)
-  }
-  else if (usermoneydata.money > -1 && usermoneydata.tuna === 1) {
-    usermoneydata.tuna = 0;
-    message.member.roles.remove('798570033235755029');
-    const webhook = new WebhookClient('798186603235246111', 'Rf6vyUbm7GuwLOmmHseu-QZp7bV7QOYykwEoqzrSLX3Rjkza_7ipOsbJQGe9BKoGNiHn');
-    webhook.send(`${message.author}、確かに借金は返してもらった、もう二度と借金すんじゃねえぞ。`);
-  }
-
   db.MoneySet(usermoneydata);
   db.DailySet(userdailydata);
 
-  if (message.channel.parentID === '798186356105936956') {
-    message.member.roles.add('798533393448960020');
-  }
-  else if (message.channel.parentID === '801057223139917884') {
+  if (message.channel.parentID === '801057223139917884') {
     message.member.roles.add('801796340057112589');
   }
 
