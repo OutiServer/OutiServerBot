3b17f2b6a210c2649688bf3601fc706d6c0ca6e7
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-22 23:58:51 +0900

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -32,7 +32,7 @@ module.exports = {
 
             ctx.font = '50px Impact';
             ctx.rotate(0);
-            ctx.fillText(`${user.username}\n${userleveldata.level}level ${userleveldata.point}/${53 * userleveldata.level}xp`, 500, 200);
+            ctx.fillText(`${user.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, 500, 200);
             const canvasDataUrl = canvas.toDataURL();
             const decoded = dataUriToBuffer(canvasDataUrl);
 
