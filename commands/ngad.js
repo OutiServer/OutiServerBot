f2aada5b8e77a478ebdd51ddf87d9555c8cd7098
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-25 15:42:05 +0900

    Create ngad.js

---
+++ commands/ngad.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const { Message } = require('discord.js');
+const all_ngwords = require('../dat/json/all_ngwords.json');
+
+module.exports = {
+    info: {
+        name: "ngadd",
+        description: "NGワード追加",
+        usage: "",
+        aliases: [""],
+        owneronly: false,
+        adminonly: true,
+        category: 'Admin'
+    },
+
+    /**
+     * @param {*} client 
+     * @param {Message} message 
+     */
+
+    run: async function (client, message, args) {
+        const addng_word = args[0];
+
+        if (!addng_word) {
+            message.react('816282137065947136');
+            return message.reply('第一引数に追加するNGワードを入れてください！');
+        }
+        fs.writeFile('../dat/json/all_ngwords.json', JSON.stringify(all_ngwords, null, ' '), (err) => {
+            if (err) {
+                console.log(err);
+                return message.channel.send(err, { code: true });
+            }
+
+            message.channel.send(`${addng_word}をNGワードに追加しました！`);
+        });
+    }
+}
