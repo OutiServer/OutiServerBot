00f72fc8588f6197e6d7c143b4d10bbffd116ec2
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-25 23:18:26 +0900

    Update guildMemberRemove.js

--- events/discord/guildMemberRemove.js
+++ events/discord/guildMemberRemove.js
@@ -1,4 +1,6 @@
 const { Client, GuildMember } = require('discord.js');
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('unkoserver.db');
 
 /**
  * @param {Client} client
@@ -8,5 +10,6 @@ const { Client, GuildMember } = require('discord.js');
 
 module.exports = async (client, member) => {
     if (member.user.bot === true || member.guild.id !== '706452606918066237') return;
+    sql.prepare('DELETE FROM  levels WHERE user = ? AND guild = ?;').run(member.id, member.guild.id);
     client.channels.cache.get('706459931351711775').send(`${member.user.tag} マサラタウンにさよならばいばい`);
 };
