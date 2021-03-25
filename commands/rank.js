974d34867bca5a00e5ff3985408802ce2a7789f8
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-25 22:39:26 +0900

    jsondata

--- commands/rank.js
+++ commands/rank.js
@@ -31,7 +31,7 @@ module.exports = {
             const userleveldata = db.levelget(user.id, message.guild.id);
             const canvas = createCanvas(1500, 700);
             const ctx = canvas.getContext('2d');
-            const rankimagedata = rankimage['714455926970777602'];
+            const rankimagedata = rankimage[user.id];
 
             if (!rankimagedata) {
                 ctx.font = '50px Impact';
@@ -93,76 +93,6 @@ module.exports = {
                         });
 
                 });
-            }
-
-            if (user.id === '714455926970777602') {
-
-            }
-            else if (user.id === '440863982413283342') {
-                ctx.font = '30px Impact';
-                ctx.rotate(0);
-                ctx.fillText(`${user.username}`, 420, 30);
-                ctx.fillText(`${userleveldata.level} level ${userleveldata.xp} /${55 * userleveldata.level}xp`, 250, 240);
-                const canvasDataUrl = canvas.toDataURL();
-                const decoded = dataUriToBuffer(canvasDataUrl);
-
-                fs.writeFile('./dat/images/level.png', decoded, (err) => {
-                    if (err) return console.log(err);
-
-                    var images = [`./dat/images/${user.id}.png`, './dat/images/level.png'];
-                    var jimps = [];
-                    for (var i = 0; i < images.length; i++) {
-                        jimps.push(jimp.read(images[i]));
-                    }
-
-                    Promise.all(jimps)
-                        .then(function () {
-                            return Promise.all(jimps);
-                        })
-                        .then(function (data) {
-                            data[0].composite(data[1], 0, 0);
-                            data[0].write('./dat/images/rank.png', function () {
-                                message.channel.send(new MessageAttachment('./dat/images/rank.png'));
-                                message.channel.stopTyping();
-                            });
-                        });
-
-                });
-            }
-            else if (user.id === '515443335012286465') {
-                ctx.font = '80px Impact';
-                ctx.fillStyle = '#ffffff';
-                ctx.rotate(0);
-                ctx.fillText(`${user.username}`, 420, 100);
-                ctx.fillText(`${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, 500, 450);
-                const canvasDataUrl = canvas.toDataURL();
-                const decoded = dataUriToBuffer(canvasDataUrl);
-
-                fs.writeFile('./dat/images/level.png', decoded, (err) => {
-                    if (err) return console.log(err);
-
-                    var images = [`./dat/images/${user.id}.png`, './dat/images/level.png'];
-                    var jimps = [];
-                    for (var i = 0; i < images.length; i++) {
-                        jimps.push(jimp.read(images[i]));
-                    }
-
-                    Promise.all(jimps)
-                        .then(function () {
-                            return Promise.all(jimps);
-                        })
-                        .then(function (data) {
-                            data[0].composite(data[1], 0, 0);
-                            data[0].write('./dat/images/rank.png', function () {
-                                message.channel.send(new MessageAttachment('./dat/images/rank.png'));
-                                message.channel.stopTyping();
-                            });
-                        });
-
-                });
-            }
-            else {
-
             }
         }
         else {
@@ -231,103 +161,6 @@ module.exports = {
                         });
 
                 });
-            }
-            if (message.author.id === '714455926970777602') {
-                ctx.font = '30px Impact';
-                ctx.fillStyle = '#00f70c';
-                ctx.rotate(0);
-                ctx.fillText(`${message.author.username}`, 5, 30);
-                ctx.fillText(`${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, 250, 370);
-                const canvasDataUrl = canvas.toDataURL();
-                const decoded = dataUriToBuffer(canvasDataUrl);
-
-                fs.writeFile('./dat/images/level.png', decoded, (err) => {
-                    if (err) return console.log(err);
-
-                    var images = [`./dat/images/${message.author.id}.png`, './dat/images/level.png'];
-                    var jimps = [];
-                    for (var i = 0; i < images.length; i++) {
-                        jimps.push(jimp.read(images[i]));
-                    }
-
-                    Promise.all(jimps)
-                        .then(function () {
-                            return Promise.all(jimps);
-                        })
-                        .then(function (data) {
-                            data[0].composite(data[1], 0, 0);
-                            data[0].write('./dat/images/rank.png', function () {
-                                message.channel.send(new MessageAttachment('./dat/images/rank.png'));
-                                message.channel.stopTyping();
-                            });
-                        });
-
-                });
-            }
-            else if (message.author.id === '440863982413283342') {
-                ctx.font = '30px Impact';
-                ctx.rotate(0);
-                ctx.fillText(`${message.author.username}`, 420, 30);
-                ctx.fillText(`${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, 250, 240);
-                const canvasDataUrl = canvas.toDataURL();
-                const decoded = dataUriToBuffer(canvasDataUrl);
-
-                fs.writeFile('./dat/images/level.png', decoded, (err) => {
-                    if (err) return console.log(err);
-
-                    var images = [`./dat/images/${message.author.id}.png`, './dat/images/level.png'];
-                    var jimps = [];
-                    for (var i = 0; i < images.length; i++) {
-                        jimps.push(jimp.read(images[i]));
-                    }
-
-                    Promise.all(jimps)
-                        .then(function () {
-                            return Promise.all(jimps);
-                        })
-                        .then(function (data) {
-                            data[0].composite(data[1], 0, 0);
-                            data[0].write('./dat/images/rank.png', function () {
-                                message.channel.send(new MessageAttachment('./dat/images/rank.png'));
-                                message.channel.stopTyping();
-                            });
-                        });
-
-                });
-            }
-            else if (message.author.id === '515443335012286465') {
-                ctx.font = '80px Impact';
-                ctx.fillStyle = '#ffffff';
-                ctx.rotate(0);
-                ctx.fillText(`${message.author.username}`, 420, 100);
-                ctx.fillText(`${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, 500, 450);
-                const canvasDataUrl = canvas.toDataURL();
-                const decoded = dataUriToBuffer(canvasDataUrl);
-
-                fs.writeFile('./dat/images/level.png', decoded, (err) => {
-                    if (err) return console.log(err);
-
-                    var images = [`./dat/images/${message.author.id}.png`, './dat/images/level.png'];
-                    var jimps = [];
-                    for (var i = 0; i < images.length; i++) {
-                        jimps.push(jimp.read(images[i]));
-                    }
-
-                    Promise.all(jimps)
-                        .then(function () {
-                            return Promise.all(jimps);
-                        })
-                        .then(function (data) {
-                            data[0].composite(data[1], 0, 0);
-                            data[0].write('./dat/images/rank.png', function () {
-                                message.channel.send(new MessageAttachment('./dat/images/rank.png'));
-                                message.channel.stopTyping();
-                            });
-                        });
-                });
-            }
-            else {
-
             }
         }
 
