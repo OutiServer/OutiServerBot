91a3bb90931e0271da38869ae14e6cfcefbe9c7b
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-26 17:52:27 +0900

    rank

--- commands/rank.js
+++ commands/rank.js
@@ -28,42 +28,83 @@ module.exports = {
             const userleveldata = db.levelget(user.id, message.guild.id);
             const rankimagedata = rankimage[user.id];
 
-            const canvas = createCanvas(1500, 500);
-            const ctx = canvas.getContext('2d');
-            const background = await loadImage('./dat/images/default.png');
-            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-            ctx.font = '80px Impact';
-            ctx.rotate(0);
-            ctx.fillText(`${user.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, canvas.width / 2.5, canvas.height / 1.8);
-            ctx.beginPath();
-            ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.clip();
-            const avatar = await loadImage(user.displayAvatarURL({ format: 'jpg' }));
-            ctx.drawImage(avatar, 25, 25, 200, 200);
-            message.channel.send(new MessageAttachment(canvas.toBuffer(), 'rank.png'));
-            message.channel.stopTyping();
-
+            if (!rankimagedata) {
+                const canvas = createCanvas(1500, 500);
+                const ctx = canvas.getContext('2d');
+                const background = await loadImage('./dat/images/default.png');
+                ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+                ctx.font = '80px Impact';
+                ctx.rotate(0);
+                ctx.fillText(`${user.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, canvas.width / 2.5, canvas.height / 1.8);
+                ctx.beginPath();
+                ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+                ctx.closePath();
+                ctx.clip();
+                const avatar = await loadImage(user.displayAvatarURL({ format: 'jpg' }));
+                ctx.drawImage(avatar, 25, 25, 200, 200);
+                message.channel.send(new MessageAttachment(canvas.toBuffer(), 'rank.png'));
+                message.channel.stopTyping();
+            }
+            else {
+                const canvas = createCanvas(rankimagedata.imagex, rankimagedata.imagey);
+                const ctx = canvas.getContext('2d');
+                const background = await loadImage(`./dat/images/${user.id}.png`);
+                ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+                ctx.font = `${rankimagedata.font}px Impact`;
+                ctx.rotate(0);
+                ctx.fillText(`${user.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, canvas.width / 2.5, canvas.height / 1.8);
+                if (rankimagedata.icon) {
+                    ctx.beginPath();
+                    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+                    ctx.closePath();
+                    ctx.clip();
+                    const avatar = await loadImage(user.displayAvatarURL({ format: 'jpg' }));
+                    ctx.drawImage(avatar, 25, 25, 200, 200);
+                }
+                message.channel.send(new MessageAttachment(canvas.toBuffer(), 'rank.png'));
+                message.channel.stopTyping();
+            }
         }
         else {
             const userleveldata = db.levelget(message.author.id, message.guild.id);
             const rankimagedata = rankimage[message.author.id];
 
-            const canvas = createCanvas(1500, 500);
-            const ctx = canvas.getContext('2d');
-            const background = await loadImage('./dat/images/default.png');
-            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-            ctx.font = '80px Impact';
-            ctx.rotate(0);
-            ctx.fillText(`${message.author.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, canvas.width / 2.5, canvas.height / 1.8);
-            ctx.beginPath();
-            ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.clip();
-            const avatar = await loadImage(message.author.displayAvatarURL({ format: 'jpg' }));
-            ctx.drawImage(avatar, 25, 25, 200, 200);
-            message.channel.send(new MessageAttachment(canvas.toBuffer(), 'rank.png'));
-            message.channel.stopTyping();
+            if (!rankimagedata) {
+                const canvas = createCanvas(1500, 500);
+                const ctx = canvas.getContext('2d');
+                const background = await loadImage('./dat/images/default.png');
+                ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+                ctx.font = '80px Impact';
+                ctx.rotate(0);
+                ctx.fillText(`${message.author.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, canvas.width / 2.5, canvas.height / 1.8);
+                ctx.beginPath();
+                ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+                ctx.closePath();
+                ctx.clip();
+                const avatar = await loadImage(message.author.displayAvatarURL({ format: 'jpg' }));
+                ctx.drawImage(avatar, 25, 25, 200, 200);
+                message.channel.send(new MessageAttachment(canvas.toBuffer(), 'rank.png'));
+                message.channel.stopTyping();
+            }
+            else {
+                const canvas = createCanvas(rankimagedata.imagex, rankimagedata.imagey);
+                const ctx = canvas.getContext('2d');
+                const background = await loadImage(`./dat/images/${message.author.id}.png`);
+                ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+                ctx.font = `${rankimagedata.font}px Impact`;
+                ctx.rotate(0);
+                ctx.fillText(`${message.author.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, canvas.width / 2.5, canvas.height / 1.8);
+                if (rankimagedata.icon) {
+                    ctx.beginPath();
+                    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+                    ctx.closePath();
+                    ctx.clip();
+                    const avatar = await loadImage(user.displayAvatarURL({ format: 'jpg' }));
+                    ctx.drawImage(avatar, 25, 25, 200, 200);
+                }
+                message.channel.send(new MessageAttachment(canvas.toBuffer(), 'rank.png'));
+                message.channel.stopTyping();
+            }
         }
 
     }
