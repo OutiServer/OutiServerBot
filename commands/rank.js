df29461c887d9f4bdee066074c4948460a038c81
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-26 13:31:23 +0900

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -29,42 +29,27 @@ module.exports = {
 
         if (user) {
             const userleveldata = db.levelget(user.id, message.guild.id);
-            const canvas = createCanvas(1500, 700);
-            const ctx = canvas.getContext('2d');
             const rankimagedata = rankimage[user.id];
 
             if (!rankimagedata) {
+                const canvas = createCanvas(1500, 500);
+                const ctx = canvas.getContext('2d');
+                const background = await loadImage('./dat/images/default.png');
+                ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
                 ctx.font = '80px Impact';
                 ctx.rotate(0);
+                ctx.beginPath();
+                ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+                ctx.closePath();
+                ctx.clip();
                 const avatar = await loadImage(user.displayAvatarURL({ format: 'jpg' }));
                 ctx.drawImage(avatar, 25, 25, 200, 200);
-                ctx.fillText(`${user.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, 500, 200);
-                const canvasDataUrl = canvas.toDataURL();
-                const decoded = dataUriToBuffer(canvasDataUrl);
-
-                fs.writeFile('./dat/images/level.png', decoded, (err) => {
-                    if (err) return console.log(err);
-
-                    var images = ['./dat/images/default.png', './dat/images/level.png'];
-                    var jimps = [];
-                    for (var i = 0; i < images.length; i++) {
-                        jimps.push(jimp.read(images[i]));
-                    }
-
-                    Promise.all(jimps)
-                        .then(function () {
-                            return Promise.all(jimps);
-                        })
-                        .then(function (data) {
-                            data[0].composite(data[1], 0, 0);
-                            data[0].write('./dat/images/rank.png', function () {
-                                message.channel.send(new MessageAttachment('./dat/images/rank.png'));
-                                message.channel.stopTyping();
-                            });
-                        });
-                });
+                ctx.fillText(`${user.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, canvas.width / 2.5, canvas.height / 1.8);
+                message.channel.send(new MessageAttachment(canvas.toBuffer(), 'rank.png'));
             }
             else {
+                const canvas = createCanvas(1500, 500);
+                const ctx = canvas.getContext('2d');
                 ctx.font = `${rankimagedata.font}px Impact`;
                 ctx.fillStyle = rankimagedata.fillStyle;
                 ctx.rotate(0);
@@ -99,42 +84,29 @@ module.exports = {
         }
         else {
             const userleveldata = db.levelget(message.author.id, message.guild.id);
-            const canvas = createCanvas(1500, 700);
-            const ctx = canvas.getContext('2d');
+            //const canvas = createCanvas(1500, 700);
+
             const rankimagedata = rankimage[message.author.id];
 
             if (!rankimagedata) {
+                const canvas = createCanvas(1500, 500);
+                const ctx = canvas.getContext('2d');
+                const background = await loadImage('./dat/images/default.png');
+                ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
                 ctx.font = '80px Impact';
                 ctx.rotate(0);
+                ctx.beginPath();
+                ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+                ctx.closePath();
+                ctx.clip();
                 const avatar = await loadImage(message.author.displayAvatarURL({ format: 'jpg' }));
                 ctx.drawImage(avatar, 25, 25, 200, 200);
-                ctx.fillText(`${message.author.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, 500, 200);
-                const canvasDataUrl = canvas.toDataURL();
-                const decoded = dataUriToBuffer(canvasDataUrl);
-
-                fs.writeFile('./dat/images/level.png', decoded, (err) => {
-                    if (err) return console.log(err);
-
-                    var images = ['./dat/images/default.png', './dat/images/level.png'];
-                    var jimps = [];
-                    for (var i = 0; i < images.length; i++) {
-                        jimps.push(jimp.read(images[i]));
-                    }
-
-                    Promise.all(jimps)
-                        .then(function () {
-                            return Promise.all(jimps);
-                        })
-                        .then(function (data) {
-                            data[0].composite(data[1], 0, 0);
-                            data[0].write('./dat/images/rank.png', function () {
-                                message.channel.send(new MessageAttachment('./dat/images/rank.png'));
-                                message.channel.stopTyping();
-                            });
-                        });
-                });
+                ctx.fillText(`${message.author.username}\n${userleveldata.level}level ${userleveldata.xp}/${55 * userleveldata.level}xp`, canvas.width / 2.5, canvas.height / 1.8);
+                message.channel.send(new MessageAttachment(canvas.toBuffer(), 'rank.png'));
             }
             else {
+                const canvas = createCanvas(1500, 500);
+                const ctx = canvas.getContext('2d');
                 ctx.font = `${rankimagedata.font}px Impact`;
                 ctx.fillStyle = rankimagedata.fillStyle;
                 ctx.rotate(0);
