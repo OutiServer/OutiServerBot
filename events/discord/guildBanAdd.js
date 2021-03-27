b537f37c312adee472207f28650a69f5102dda67
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-28 01:57:11 +0900

    Update guildBanAdd.js

--- events/discord/guildBanAdd.js
+++ events/discord/guildBanAdd.js
@@ -7,7 +7,7 @@ const { Client, Guild, User } = require('discord.js');
  */
 
 module.exports = async (client, guild, user) => {
-    if (guild.id !== '706452606918066237') return;
+    if (user.bot || guild.id !== '706452606918066237') return;
 
     const replys = ['がBANされましたwwwwwwwwwwwww', 'はシベリア旅行に行った', 'は BABANBABANBANBA BABANBABANBANBAN された', 'は下水処理場へ流れていった。', 'は豚箱に送られた', 'は粛清された', 'は北に拉致された', 'は自粛した'];
     const random = Math.floor(Math.random() * replys.length);
