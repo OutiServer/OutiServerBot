b44d9857a22b85cfdd9301819e02613ff3826053
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-28 01:56:40 +0900

    Update guildBanAdd.js

--- events/discord/guildBanAdd.js
+++ events/discord/guildBanAdd.js
@@ -8,8 +8,10 @@ const { Client, Guild, User } = require('discord.js');
 
 module.exports = async (client, guild, user) => {
     if (guild.id !== '706452606918066237') return;
+
     const replys = ['がBANされましたwwwwwwwwwwwww', 'はシベリア旅行に行った', 'は BABANBABANBANBA BABANBABANBANBAN された', 'は下水処理場へ流れていった。', 'は豚箱に送られた', 'は粛清された', 'は北に拉致された', 'は自粛した'];
-    let random = Math.floor(Math.random() * replys.length);
+    const random = Math.floor(Math.random() * replys.length);
+
     client.channels.cache.get('706459931351711775').send(user.tag + replys[random]);
-    client.channels.cache.get('706452607538954263').send(`${user.tag}がゲームから追放されました <:emoji_106:790546684710223882>`);
+    client.channels.cache.get('706452607538954263').send(`${user.tag} has been banned <:emoji_106:790546684710223882>`);
 };
