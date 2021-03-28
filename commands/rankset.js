7a4e6c8239c63cf788211b17901daf39fe01c104
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-28 20:48:11 +0900

    Create rankset.js

---
+++ commands/rankset.js
@@ -0,0 +1,11 @@
+module.exports = {
+    info: {
+        name: "request",
+        description: "rank画像リクエスト",
+        usage: "",
+        aliases: [""],
+        owneronly: false,
+        adminonly: false,
+        category: 'Level'
+    },
+}
