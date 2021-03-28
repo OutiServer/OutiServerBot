7dc51179be7a049a071e76f8b2c01d2ef1819010
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-28 20:43:40 +0900

    Update voiceStateUpdate.js

--- events/discord/voiceStateUpdate.js
+++ events/discord/voiceStateUpdate.js
@@ -13,12 +13,18 @@ module.exports = async (client, oldMember, newMember) => {
   }
   if (oldMember.guild.id !== '706452606918066237' || oldMember.member.user.bot) return;
   if (oldMember.channelID === null) {
+    if (newMember.channelID === '822310255324299294') return client.channels.cache.get('825674456470519809').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
     client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
   }
   else if (newMember.channelID === null) {
+    if (oldMember.channelID === '822310255324299294') return client.channels.cache.get('825674456470519809').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から退出しました`);
     client.channels.cache.get('706458716320432198').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から退出しました`);
   }
   else if (newMember.channelID !== oldMember.channelID) {
+    if (oldMember.channelID === '822310255324299294') {
+      client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
+      return client.channels.cache.get('825674456470519809').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から退出しました`);
+    }
     client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${oldMember.channel.name}から${newMember.channel.name}に移動しました`);
   }
 };
