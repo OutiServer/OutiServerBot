aec85c3f2a73b63a0eb5b2fe5d53d40e9f567d2a
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-28 18:33:20 +0900

    Update request.js

--- commands/request.js
+++ commands/request.js
@@ -1,8 +1,6 @@
-const fs = require('fs');
 const request = require('request');
 const { Message, Client } = require("discord.js");
 const { Database } = require('../unko/index');
-const rankimage = require('../dat/json/rankimage.json');
 const db = new Database('unkoserver.db');
 
 module.exports = {
@@ -41,21 +39,16 @@ module.exports = {
                 },
                 function (error, response, body) {
                     if (!error && response.statusCode === 200) {
-                        fs.writeFileSync(`./dat/images/${message.author.id}.png`, body, 'binary');
-                        rankimage[message.author.id] = {
-                            "font": 80,
-                            "fillStyle": "#000000",
-                            "imagex": attachment.width,
-                            "imagey": attachment.height
+                        const data = {
+                            id: `${message.author.id}`,
+                            user: message.author.id,
+                            font: 80,
+                            fillstyle: '#000000',
+                            imagex: attachment.width,
+                            imagey: attachment.height,
+                            icon: 1
                         }
-                        fs.writeFile('./dat/json/rankimage.json', JSON.stringify(rankimage, null, ' '), (err) => {
-                            if (err) {
-                                console.log(err);
-                                return message.channel.send(err, { code: true });
-                            }
-
-                            message.channel.send('level画像を設定しました！');
-                        });
+                        db.Rankimageset(data);
                     }
                 }
             );
