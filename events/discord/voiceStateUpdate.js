fe798841ede732656309a92d52afae2d684febc9
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-28 22:29:24 +0900

    Update voiceStateUpdate.js

--- events/discord/voiceStateUpdate.js
+++ events/discord/voiceStateUpdate.js
@@ -1,9 +1,9 @@
-const { Client, GuildMember } = require('discord.js');
+const { Client, VoiceState } = require('discord.js');
 
 /**
  * @param {Client} client
- * @param {GuildMember} oldMember
- * @param {GuildMember} newMember
+ * @param {VoiceState} oldMember
+ * @param {VoiceState} newMember
  */
 
 module.exports = async (client, oldMember, newMember) => {
@@ -11,20 +11,37 @@ module.exports = async (client, oldMember, newMember) => {
   if (conn && conn.channel && conn.channel.members.array().length < 2) {
     conn.disconnect();
   }
+
   if (oldMember.guild.id !== '706452606918066237' || oldMember.member.user.bot) return;
   if (oldMember.channelID === null) {
-    if (newMember.channelID === '822310255324299294') return client.channels.cache.get('825674456470519809').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
-    client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
+    if (newMember.channelID === '822310255324299294') {
+      client.channels.cache.get('825674456470519809').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
+    }
+    else {
+      client.channels.cache.get('706458716320432198').setTopic(`${newMember.channel.name}のVC接続数: ${newMember.channel.members.size}人`);
+      client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
+    }
   }
   else if (newMember.channelID === null) {
-    if (oldMember.channelID === '822310255324299294') return client.channels.cache.get('825674456470519809').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から退出しました`);
-    client.channels.cache.get('706458716320432198').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から退出しました`);
+    if (oldMember.channelID === '822310255324299294') {
+      client.channels.cache.get('825674456470519809').setTopic(`${oldMember.channel.name}のVC接続数: ${oldMember.channel.members.size}人`);
+      client.channels.cache.get('825674456470519809').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から退出しました`);
+    }
+    else {
+      client.channels.cache.get('706458716320432198').setTopic(`${oldMember.channel.name}のVC接続数: ${oldMember.channel.members.size}人`);
+      client.channels.cache.get('706458716320432198').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から退出しました`);
+    }
   }
   else if (newMember.channelID !== oldMember.channelID) {
-    if (oldMember.channelID === '822310255324299294') {
-      client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${newMember.channel.name}に入室しました`);
-      return client.channels.cache.get('825674456470519809').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から退出しました`);
+    if (oldMember.channelID === '822310255324299294' || newMember.channelID === '822310255324299294') {
+      client.channels.cache.get('825674456470519809').setTopic(`${oldMember.channel.name}のVC接続数: ${oldMember.channel.members.size}人`);
+      client.channels.cache.get('706458716320432198').setTopic(`${oldMember.channel.name}のVC接続数: ${oldMember.channel.members.size}人`);
+      client.channels.cache.get('706458716320432198').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から${newMember.channel.name}に移動しました`);
+      client.channels.cache.get('825674456470519809').send(`${oldMember.member.user.tag}が${oldMember.channel.name}から${newMember.channel.name}に移動しました`);
     }
-    client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${oldMember.channel.name}から${newMember.channel.name}に移動しました`);
+    else {
+      client.channels.cache.get('706458716320432198').setTopic(`${oldMember.channel.name}のVC接続数: ${oldMember.channel.members.size}人`);
+      client.channels.cache.get('706458716320432198').send(`${newMember.member.user.tag}が${oldMember.channel.name}から${newMember.channel.name}に移動しました`);
+    }
   }
 };
