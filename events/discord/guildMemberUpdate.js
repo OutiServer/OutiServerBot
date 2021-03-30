1d31aff35f9f49cffe2c38211f47fdea5246d68f
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-30 19:05:03 +0900

    Update guildMemberUpdate.js

--- events/discord/guildMemberUpdate.js
+++ events/discord/guildMemberUpdate.js
@@ -8,6 +8,6 @@ const { Client, GuildMember } = require("discord.js");
 
 module.exports = async (client, oldMember, newMember) => {
     if (!oldMember.roles.cache.has('821715178147020800') && newMember.roles.cache.has('821715178147020800')) {
-        client.channels.cache.get('825231334657884161').send(`${newMember} 認証されました、ようこそ！うんこ鯖へ！`);
+        client.channels.cache.get('825231334657884161').send(`${newMember} 認証されました、ようこそ！おうち鯖へ！`);
     }
 }
