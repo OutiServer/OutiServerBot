d8e20bfcd49ffbf5441b604cd71a0d46c4eff03b
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-31 14:54:50 +0900

    Update messageUpdate.js

--- events/discord/messageUpdate.js
+++ events/discord/messageUpdate.js
@@ -10,9 +10,7 @@ module.exports = (client, oldMessage, newMessage) => {
     client.channels.cache.get('825394905572573184').send(
         new MessageEmbed()
             .setTitle('メッセージが編集されました')
-            .addField('User', message.author.tag)
-            .addField('oldMessage', oldMessage.content)
-            .addField('newMessage', newMessage.content)
+            .setDescription(`User: ${oldMessage.author.tag}\noldMessage: ${oldMessage.content}\nnewMessage: ${newMessage.content}`)
             .setColor('RANDOM')
             .setThumbnail()
     );
