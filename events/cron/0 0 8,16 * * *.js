8bede47f0ab5c095c3e63fa09804ed865cee7e69
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-31 23:26:20 +0900

    Update 0 0 8,16 * * *.js

--- events/cron/0 0 8,16 * * *.js
+++ events/cron/0 0 8,16 * * *.js
@@ -5,9 +5,10 @@ const { Client, MessageEmbed } = require('discord.js');
  */
 
 module.exports = (client) => {
-    client.channels.cache.get('706452607538954263').send(
+    client.channels.cache.get('825231334657884161').send(
         new MessageEmbed()
-            .setDescription(`匿名で参加できるアンケートを設置しています。暇なときに記入してみてください。貴重な意見を待っています。\n[うんこ鯖アンケート](https://forms.gle/aRtBT1piAofz3vJM6)\n[みんなが遊びたい理想の鯖とは](https://docs.google.com/forms/d/156rdFiJkwUzNsHvx9KBNnEdoTFvJINsABn7x6hP8vzw/edit)\n[:new:アンケート:new:](https://docs.google.com/forms/d/e/1FAIpQLScJNd7ka66_69_irUeDr6NPAf5t4z_oNOKFWs60UBctGWkePA/viewform?usp=sf_link)`)
+            .setDescription(`匿名で参加できるアンケートを設置しています。暇なときに記入してみてください。貴重な意見を待っています。\n[みんなが遊びたい理想の鯖とは](https://docs.google.com/forms/d/156rdFiJkwUzNsHvx9KBNnEdoTFvJINsABn7x6hP8vzw/edit)\n[うんこ鯖botからの質問](https://docs.google.com/forms/d/e/1FAIpQLScJNd7ka66_69_irUeDr6NPAf5t4z_oNOKFWs60UBctGWkePA/viewform?usp=sf_link)`)
+            .setImage('https://media.discordapp.net/attachments/818411667015991297/826376437769568286/outisabakoiyo.png')
             .setColor('RANDOM')
             .setTimestamp()
     );
