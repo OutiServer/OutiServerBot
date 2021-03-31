45cc60af800b18b5468b549b875ddb83eea3cc2b
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-31 23:28:15 +0900

    Update guildMemberAdd.js

--- events/discord/guildMemberAdd.js
+++ events/discord/guildMemberAdd.js
@@ -8,5 +8,5 @@ const { Client, GuildMember } = require('discord.js');
 module.exports = async (client, member) => {
     if (member.user.bot || member.guild.id !== '706452606918066237') return;
 
-    client.channels.cache.get('797008715646500865').send(`${member}さん、<#825536134054543412>の記入とアンケートの参加お願いします。\nhttps://docs.google.com/forms/d/e/1FAIpQLScJNd7ka66_69_irUeDr6NPAf5t4z_oNOKFWs60UBctGWkePA/viewform?usp=sf_link\nもし抜ける場合はこちらを抜ける前に記入していただけると幸いです\nhttps://docs.google.com/forms/d/166hmvv-eYtmazsENRWvl9i5JrCiDe5TnxRnyEB5VhmA/edit`);
+    client.channels.cache.get('797008715646500865').send(`${member}さん、よろしくお願いします。\n<#825536134054543412>の記入お願いします。`);
 };
