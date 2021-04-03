352a6207257376f0ca152d198fa88f20946e9e54
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-04-03 18:28:33 +0900

    logをおうち鯖のみに

--- events/discord/messageUpdate.js
+++ events/discord/messageUpdate.js
@@ -7,7 +7,7 @@ const { Client, Message, MessageEmbed } = require('discord.js');
  */
 
 module.exports = (client, oldMessage, newMessage) => {
-    if (oldMessage.guild.id === '775952658779209770' || oldMessage.author.bot) return;
+    if (oldMessage.guild.id !== '706452606918066237' || oldMessage.author.bot) return;
     client.channels.cache.get('825394905572573184').send(
         new MessageEmbed()
             .setTitle('メッセージが編集されました')
