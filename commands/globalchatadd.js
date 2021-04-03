b3ddd0a331c4af503e80e3c9837a095de23ed000
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-04-03 17:37:57 +0900

    globalチャットチャンネル追加コマンド修正

--- commands/globalchatadd.js
+++ commands/globalchatadd.js
@@ -22,6 +22,6 @@ module.exports = {
     run: async function (client, message, args) {
         const channel = client.channels.cache.get(args[0]);
         if (!channel) return message.reply('第一引数にグローバルチャットに追加するチャンネルIDを入れてください')
-        db.globalchatset(message.guild.id, channel);
+        db.globalchatset(message.guild.id, channel.id);
     }
 }
