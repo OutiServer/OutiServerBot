3ddcbb3a7dd7fc4c5c3cce665c88fc155d825064
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-04-04 22:44:37 +0900

    認証されていないユーザーへのメンションコマンド追加

---
+++ commands/mention.js
@@ -0,0 +1,32 @@
+const { Client, Message } = require("discord.js");
+
+module.exports = {
+    info: {
+        name: "mention",
+        description: "ホワイトリストに書いていない人にメンションを飛ばす",
+        usage: "",
+        aliases: ["m"],
+        owneronly: false,
+        adminonly: true,
+        category: 'Admin'
+    },
+
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {Array} args 
+     */
+
+    run: async function (client, message, args) {
+        const allusers = message.guild.members.cache.map(user => user.id);
+        const notrolesusers = [];
+
+        for (const data of allusers) {
+            if (!message.guild.member(data).roles.cache.has('821715178147020800')) {
+                notrolesusers.push('<@' + data + '>');
+            }
+        }
+
+        client.channels.cache.get('797008715646500865').send(notrolesusers.join('\n') + '\n<#825536134054543412>のグーグルフォームへの記入お願いします。\nわからない場合はこのチャンネルで聞いてください！');
+    }
+}
