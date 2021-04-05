e4d6ba609984368216137b24c8805214aeef776a
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-04-05 21:51:47 +0900

    スレッド再オープンコマンド追加

---
+++ commands/open.js
@@ -0,0 +1,27 @@
+const { Client, Message } = require("discord.js");
+const { Database } = require('../unko/index');
+const db = new Database('unkoserver.db');
+
+module.exports = {
+    info: {
+        name: "open",
+        description: "スレッド再オープン",
+        usage: "",
+        aliases: [""],
+        owneronly: false,
+        adminonly: true,
+        category: 'Main'
+    },
+
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {Array} args 
+     */
+
+    run: async function (client, message, args) {
+        message.channel.setParent('828266382277345310');
+
+        message.channel.send('このスレッドを再オープンしました。');
+    }
+}
