06f834fbce06e79b86b0fdc1a795121fe2152ee3
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-04-09 20:19:35 +0900

    国コマンド大幅改良

---
+++ commands/countryadd.js
@@ -0,0 +1,39 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const { Database } = require('../home/index');
+
+module.exports = {
+    info: {
+        name: "countryadd",
+        description: "国追加",
+        usage: "[リーダーのユーザーID] [国ロールID]",
+        aliases: [""],
+        owneronly: false,
+        adminonly: true,
+        category: 'Admin'
+    },
+
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {Array} args 
+     */
+
+    run: async function (client, message, args) {
+        const leader = message.guild.member(args[0]);
+        const role = message.guild.roles.cache.get(args[1]);
+        if (!leader || !role) return message.reply('第一引数に国リーダーのユーザーID、第二引数に国ロールIDを入れてください！');
+
+        const db = new Database('unkoserver.db');
+
+        db.Countryset(leader.id, role.id);
+
+        message.channel.send(
+            new MessageEmbed()
+                .setTitle('国追加完了')
+                .addField('国リーダー', leader.user.tag)
+                .addField('国ロール', `<@&${role.id}>`)
+                .setColor('RANDOM')
+                .setTimestamp()
+        );
+    }
+}
--- commands/teams.js
+++ commands/teams.js
@@ -1,4 +1,5 @@
 const { Client, Message, MessageEmbed } = require("discord.js");
+const { Database } = require('../home/index');
 
 module.exports = {
     info: {
@@ -18,65 +19,19 @@ module.exports = {
      */
 
     run: async function (client, message, args) {
-        const homeserver = client.guilds.cache.get('706452606918066237');
-        const okeihan = homeserver.roles.cache.get('827888356553654313');
-        const sum = homeserver.roles.cache.get('827888505408847882');
-        const syowatan = homeserver.roles.cache.get('827888677421187092');
-        const seaside = homeserver.roles.cache.get('827916187790475306');
-        const samehada = homeserver.roles.cache.get('828059763802636289');
-        const NewCountry = homeserver.roles.cache.get('828098224828186644');
-        const nesutonia = homeserver.roles.cache.get('828486972972466236');
+        const db = new Database('unkoserver.db');
+        const all = db.Countrygetall();
         let embeds = [];
-
-        embeds.push(
-            new MessageEmbed()
-                .setTitle(okeihan.name)
-                .setDescription(`メンバー数: ${okeihan.members.size}\n\nメンバー:\n${okeihan.members.map(user => user.user.tag).join('\n')}`)
-                .setColor('RANDOM')
-                .setTimestamp()
-        );
-        embeds.push(
-            new MessageEmbed()
-                .setTitle(sum.name)
-                .setDescription(`メンバー数: ${sum.members.size}\n\nメンバー:\n${sum.members.map(user => user.user.tag).join('\n')}`)
-                .setColor('RANDOM')
-                .setTimestamp()
-        );
-        embeds.push(
-            new MessageEmbed()
-                .setTitle(syowatan.name)
-                .setDescription(`メンバー数: ${syowatan.members.size}\n\nメンバー:\n${syowatan.members.map(user => user.user.tag).join('\n')}`)
-                .setColor('RANDOM')
-                .setTimestamp()
-        );
-        embeds.push(
-            new MessageEmbed()
-                .setTitle(seaside.name)
-                .setDescription(`メンバー数: ${seaside.members.size}\n\nメンバー:\n${seaside.members.map(user => user.user.tag).join('\n')}`)
-                .setColor('RANDOM')
-                .setTimestamp()
-        );
-        embeds.push(
-            new MessageEmbed()
-                .setTitle(samehada.name)
-                .setDescription(`メンバー数: ${samehada.members.size}\n\nメンバー:\n${samehada.members.map(user => user.user.tag).join('\n')}`)
-                .setColor('RANDOM')
-                .setTimestamp()
-        );
-        embeds.push(
-            new MessageEmbed()
-                .setTitle(NewCountry.name)
-                .setDescription(`メンバー数: ${NewCountry.members.size}\n\nメンバー:\n${NewCountry.members.map(user => user.user.tag).join('\n')}`)
-                .setColor('RANDOM')
-                .setTimestamp()
-        );
-        embeds.push(
-            new MessageEmbed()
-                .setTitle(nesutonia.name)
-                .setDescription(`メンバー数: ${nesutonia.members.size}\n\nメンバー:\n${nesutonia.members.map(user => user.user.tag).join('\n')}`)
-                .setColor('RANDOM')
-                .setTimestamp()
-        );
+        for (const data of all) {
+            const role = client.guilds.cache.get('706452606918066237').roles.cache.get(data.role);
+            embeds.push(
+                new MessageEmbed()
+                    .setTitle(role.name)
+                    .setDescription(`メンバー数: ${role.members.size}\n\nリーダー: ${message.guild.member(data.leader).user.tag}\nメンバー:\n${role.members.map(user => user.user.tag).join('\n')}`)
+                    .setColor('RANDOM')
+                    .setTimestamp()
+            );
+        }
 
         const msg = await message.channel.send('```' + `1/${embeds.length}ページ目を表示中\nみたいページ番号を発言してください\n0を送信するか30秒経つと処理が止まります` + '```', embeds[0]);
         while (true) {
