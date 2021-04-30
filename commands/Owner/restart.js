c84e53ccd5c20248b701a911c4d5d1c6f34fb18d
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-04-30 18:18:39 +0900

    強制再起動コマンドの追加

--- commands/Owner/restart.js
+++ commands/Owner/restart.js
@@ -1,10 +1,11 @@
 const { Client, Message } = require('discord.js');
 const { errorlog } = require('../../functions/error');
+const { execSync } = require('child_process');
 
 module.exports = {
     info: {
         name: "restart",
-        description: "再接続",
+        description: "再起動",
         usage: "",
         aliases: [""],
         owneronly: true,
@@ -19,11 +20,17 @@ module.exports = {
      */
 
     run: async function (client, message, args) {
-        message.channel.send('再接続しています...')
-            .then(() => client.destroy())
-            .then(() => client.login(process.env.DISCORD_TOKEN))
-            .then(() => message.channel.send('再接続が完了しました'))
-            .catch(error => errorlog(client, message, error))
-            .finally(() => client.cooldown.set(message.author.id, false));
+        try {
+            if (!args[0]) return message.reply('第一引数におうち鯖Botのインスタンス番号を入れてください！');
+            client.channels.cache.get('706452607538954263').send(`${message.author.tag}がおうち鯖Botの強制再起動を開始しました！`)
+                .then(msg => execSync(`forever restart ${args[0]}`));
+        } catch (error) {
+            errorlog(client, message, error);
+        }
+        finally {
+            client.cooldown.set(message.author.id, false);
+        }
+
+
     },
 };
