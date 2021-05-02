5765776aedbbbe5351a03f8aa3f1884b47a73060
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-05-02 19:08:39 +0900

    rankの文字色を16進数以外でも設定できるように

--- commands/Level/rankset.js
+++ commands/Level/rankset.js
@@ -1,4 +1,4 @@
-const { Client, Message, MessageEmbed } = require('discord.js');
+const { Client, Message, MessageEmbed, Util } = require('discord.js');
 const { errorlog } = require('../../functions/error');
 
 module.exports = {
@@ -32,38 +32,29 @@ module.exports = {
             }
             else if (command === 'color') {
                 const color = args[1];
-                if (!color) return message.reply('第二引数に文字カラーを16進数で入れてください！');
-                userrankimagedata.fillStyle = color;
+                if (!color) return message.reply('第二引数に文字カラーを入れてください！');
+                userrankimagedata.fillStyle = Util.resolveColor(color);
                 client.db.prepare('UPDATE rankimages SET fillStyle = ? WHERE id = ?').run(userrankimagedata.fillStyle, userrankimagedata.id);
                 message.channel.send(`文字カラーを${userrankimagedata.fillStyle}に設定しました！`);
             }
-            else if (command === 'imagex') {
-                const imagex = args[1];
-                if (!imagex || !Number(imagex)) return message.reply('第二引数に中央からのX座標を数値で入れてください！');
-                userrankimagedata.imagex = imagex;
-                client.db.prepare('UPDATE rankimages SET imagex = ? WHERE id = ?').run(userrankimagedata.imagex, userrankimagedata.id);
-                message.channel.send(`X座標を${userrankimagedata.imagex}に設定しました！`);
-            }
-            else if (command === 'imagey') {
-                const imagey = args[1];
-                if (!imagey || !Number(imagey)) return message.reply('第二引数に中央からのY座標を数値で入れてください！');
-                userrankimagedata.imagey = imagey;
-                client.db.prepare('UPDATE rankimages SET imagey = ? WHERE id = ?').run(userrankimagedata.imagey, userrankimagedata.id);
-                message.channel.send(`X座標を${userrankimagedata.imagey}に設定しました！`);
-            }
             else if (command === 'icon') {
                 const icon = args[1];
-                if (!icon || icon !== 'yes' && icon !== 'no') return message.reply('第二引数にiconを表示するかどうかを`yes` か `no` で入れてください！');
-                if (icon === 'yes') userrankimagedata.icon = 1;
-                else if (icon === 'no') userrankimagedata.icon = 0;
+                if (!icon || icon !== 'yes' && icon !== 'no') return message.reply('第二引数にiconを表示するかどうかを`on` か `off` で入れてください！');
+                if (icon === 'on') userrankimagedata.icon = 1;
+                else if (icon === 'off') userrankimagedata.icon = 0;
                 client.db.prepare('UPDATE rankimages SET icon = ? WHERE id = ?').run(userrankimagedata.icon, userrankimagedata.id);
-                message.channel.send(`Iconを${userrankimagedata.icon}に設定しました！`);
+                if (userrankimagedata.icon === 1) {
+                    message.channel.send(`Iconを表示するに設定しました！`);
+                }
+                else if (userrankimagedata.icon === 0) {
+                    message.channel.send(`Iconを表示しないに設定しました！`);
+                }
             }
             else {
                 message.channel.send(
                     new MessageEmbed()
                         .setTitle('ranksetで設定できる項目')
-                        .setDescription('`size`, `color`, `imagex`, `imagey`, `icon`')
+                        .setDescription('`size`, `color`, `icon`')
                 )
             }
         } catch (error) {
