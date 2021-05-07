5fb75c3a418478ed4fee4e196fd478fe2bf7fd94
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-05-07 17:22:06 +0900

    拡張子のつけ忘れ

--- events/cron/0 0 0 * * *.js
+++ events/cron/0 0 0 * * *.js
@@ -8,8 +8,8 @@ const { clienterrorlog } = require('../../functions/error');
 module.exports = (client) => {
     try {
         const time = new Date();
-        client.db.backup(`dat/backup/${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`)
-            .then(() => client.channels.cache.get('816555488694108170').send(new MessageAttachment(`dat/backup/${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`)));
+        client.db.backup(`dat/backup/${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}.backup.db`)
+            .then(() => client.channels.cache.get('816555488694108170').send(new MessageAttachment(`dat/backup/${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}.backup.db`)));
 
         client.channels.cache.get('774594290679545886').messages.fetch('794246738881019915')
             .then(msg => msg.reactions.removeAll())
