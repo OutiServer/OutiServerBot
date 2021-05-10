9a256d41355b181993eea18efa5e7ca69e5497be
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-05-10 22:01:17 +0900

    おまけコマンド追加

---
+++ commands/Omake/fuck.js
@@ -0,0 +1,31 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const { errorlog } = require('../../functions/error');
+
+module.exports = {
+    info: {
+        name: "fuck",
+        description: "fuckbird",
+        usage: "",
+        aliases: [""],
+        owneronly: false,
+        adminonly: false,
+        category: 'Omake'
+    },
+
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {Array} args 
+     */
+
+    run: async function (client, message, args) {
+        try {
+            message.channel.send('https://media.discordapp.net/attachments/840154191036022794/841298027960729671/fuck.png');
+        } catch (error) {
+            errorlog(client, message, error);
+        }
+        finally {
+            client.cooldown.set(message.author.id, false);
+        }
+    }
+}
---
+++ commands/Omake/self.js
@@ -0,0 +1,36 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const { errorlog } = require('../../functions/error');
+
+module.exports = {
+    info: {
+        name: "self",
+        description: "selfbot検知しました",
+        usage: "",
+        aliases: [""],
+        owneronly: false,
+        adminonly: false,
+        category: 'Omake'
+    },
+
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {Array} args 
+     */
+
+    run: async function (client, message, args) {
+        try {
+            message.channel.send(
+                new MessageEmbed()
+                    .setDescription(`<@${message.author.id}>さん、selfbot検知しました\n問答無用で永BANです＾＾`)
+                    .setColor('RANDOM')
+                    .setTimestamp()
+            );
+        } catch (error) {
+            errorlog(client, message, error);
+        }
+        finally {
+            client.cooldown.set(message.author.id, false);
+        }
+    }
+}
