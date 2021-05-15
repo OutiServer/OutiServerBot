c480e2489c09345ff04036e1072c12a7a34c1110
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-05-15 18:47:05 +0900

    エラー出るので取得しないように

--- events/cron/* * * * *.js
+++ events/cron/* * * * *.js
@@ -37,11 +37,11 @@ module.exports = (client) => {
                         );
                     });
             });
-            */
 
         util.statusBedrock('126.235.33.140', { port: 19131, timeout: 1000 })
             .then(result => client.channels.cache.get('834317763769925632').setTopic(`現在のサーバー参加人数: ${result.onlinePlayers}/${result.maxPlayers}人`))
             .catch();
+            */
 
         client.levelcooldown = new Collection();
     } catch (error) {
