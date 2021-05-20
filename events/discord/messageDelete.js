6ecbfccbf9796d4de0140f8374497c0291943ddc
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-05-20 17:09:17 +0900

    avatarurl

--- events/discord/messageDelete.js
+++ events/discord/messageDelete.js
@@ -9,13 +9,15 @@ const { clienterrorlog } = require("../../functions/error");
 module.exports = (client, message) => {
     try {
         if (message.guild.id !== '706452606918066237' || message.author.bot) return;
-        client.channels.cache.get('825394905572573184').send(
-            new MessageEmbed()
-                .setTitle('メッセージが削除されました')
-                .setDescription(`User: ${message.author.tag}\nMessage: ${message.content}`)
-                .setColor('RANDOM')
-                .setThumbnail()
-        );
+        if (message.author.avatar)
+            client.channels.cache.get('825394905572573184').send(
+                new MessageEmbed()
+                    .setTitle('メッセージが削除されました')
+                    .setDescription(`User: ${message.author.tag}\nMessage: ${message.content}`)
+                    .setThumbnail(message.author.avatarURL())
+                    .setColor('RANDOM')
+                    .setThumbnail()
+            );
     } catch (error) {
         clienterrorlog(client, error);
     }
