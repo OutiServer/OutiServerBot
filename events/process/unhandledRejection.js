74755b43261367d3349514c39560590ba61f2d44
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-05-20 17:08:47 +0900

    メンション

--- events/process/unhandledRejection.js
+++ events/process/unhandledRejection.js
@@ -7,7 +7,8 @@ const { Client, WebhookClient } = require('discord.js');
 module.exports = (client, reason, promise) => {
     console.error(reason);
     try {
-        const webhook = new WebhookClient('835744870114656256', '4YsFlIYycw-n_SvDq6awAQCobZ6n9CCo8IqzlZa4NiYhXvhlXLk1OSrx_BoEkf99cSqv');
+        const webhook = new WebhookClient('835806322938216468', 'KkEyBU0QDVaiiYNea_gCWrv4-ulyT-vnuKAT9tu7vWleO-JqFa6fCDV5H2BDvY8jQBPf');
+        webhook.send(`<@${process.env.OWNERID}>`);
         webhook.send(reason.stack, { code: true, split: true });
     } catch (error) { }
 };
