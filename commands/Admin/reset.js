cc80b1fd31cae55323d229d3997f3cd8962bf44f
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-05-21 17:58:45 +0900

    リセットコマンドの追加

---
+++ commands/Admin/reset.js
@@ -0,0 +1,39 @@
+const { Client, Message } = require("discord.js");
+const { errorlog } = require("../../functions/error");
+
+module.exports = {
+    info: {
+        name: "reset",
+        description: "レベルをリセット",
+        usage: "[リセットするユーザー]",
+        aliases: ["re"],
+        owneronly: false,
+        adminonly: true,
+        category: 'Admin'
+    },
+
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {Array} args 
+     */
+
+    run: async function (client, message, args) {
+        try {
+            const user = message.mentions.users.first() || client.users.cache.get(args[0]);
+            if (!user) {
+                message.react('844473484745637888');
+                message.reply('第一引数にリセットするユーザーをメンションするか、IDを入れてください！');
+                return;
+            }
+
+            client.db.prepare('DELETE FROM levels WHERE user = ?').run(user.id);
+            message.channel.send(`${user.tag}のレベルデータを削除しました`);
+        } catch (error) {
+            errorlog(client, message, error);
+        }
+        finally {
+            client.cooldown.set(message.author.id, false);
+        }
+    }
+}
