68c58f8606c9bfcdf8e9255e90f0085332537e67
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-06-03 18:57:00 +0900

    [add] メッセージ一括削除コマンドの追加

---
+++ commands/Admin/delete.js
@@ -0,0 +1,33 @@
+const { Client, Message } = require("discord.js");
+
+module.exports = {
+    info: {
+        name: "delete",
+        description: "メッセージ削除",
+        usage: "[削除するメッセージ数]",
+        aliases: [""],
+        owneronly: false,
+        adminonly: true,
+        category: 'Admin'
+    },
+
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {Array} args 
+     */
+
+    run: async function (client, message, args) {
+        try {
+            const count = Number(args[0]);
+            if (!count) return message.reply('第一引数に削除するメッセージ数を入れてください');
+            await message.channel.bulkDelete(count + 1);
+            message.channel.send(`${count} messages is deleted`);
+        } catch (error) {
+
+        }
+        finally {
+            client.cooldown.set(message.author.id, false);
+        }
+    }
+}
