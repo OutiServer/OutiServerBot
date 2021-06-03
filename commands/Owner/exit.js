ec00e20c4b45e5df6605fb4cc6a8cc770de2d297
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-06-03 19:22:21 +0900

    安全に終了するためのコマンドを追加

---
+++ commands/Owner/exit.js
@@ -0,0 +1,32 @@
+const { Client, Message } = require('discord.js');
+const exit = require('../../functions/exit');
+const { errorlog } = require('../../functions/error');
+
+module.exports = {
+    info: {
+        name: "exit",
+        description: "終了",
+        usage: "",
+        aliases: [""],
+        owneronly: true,
+        adminonly: false,
+        category: 'Owner'
+    },
+
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {Array} args
+     */
+
+    run: async function (client, message, args) {
+        try {
+            exit(client);
+        } catch (error) {
+            errorlog(client, message, error);
+        }
+        finally {
+            client.cooldown.set(message.author.id, false);
+        }
+    },
+};
