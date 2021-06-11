4ff30861feda8f2783578c611a5ebfa771a1fc69
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-06-11 18:19:41 +0900

    suumoコマンドの追加

---
+++ commands/Omake/suumo.js
@@ -0,0 +1,32 @@
+
+const { Client, Message } = require("discord.js");
+const { errorlog } = require('../../functions/error');
+
+module.exports = {
+    info: {
+        name: "suumo",
+        description: "あ❗️ スーモ❗️:new_moon_with_face:ダン:boom:ダン:boom:ダン:boom:シャーン:notes:スモ:full_moon_with_face:スモ:new_moon_with_face:スモ:full_moon_with_face:スモ:new_moon_with_face:スモ:full_moon_with_face:スモ:new_moon_with_face:ス〜〜〜モ:arrow_heading_up:スモ:new_moon_with_face:スモ:full_moon_with_face:スモ:new_moon_with_face:スモ:full_moon_with_face:スモ:new_moon_with_face:スモ:full_moon_with_face:ス～～～モ:arrow_heading_down::sun_with_face:",
+        usage: "",
+        aliases: [""],
+        owneronly: false,
+        adminonly: false,
+        category: 'Omake'
+    },
+
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {Array} args 
+     */
+
+    run: async function (client, message, args) {
+        try {
+            message.channel.send('あ❗️ スーモ❗️:new_moon_with_face:ダン:boom:ダン:boom:ダン:boom:シャーン:notes:スモ:full_moon_with_face:スモ:new_moon_with_face:スモ:full_moon_with_face:スモ:new_moon_with_face:スモ:full_moon_with_face:スモ:new_moon_with_face:ス〜〜〜モ:arrow_heading_up:スモ:new_moon_with_face:スモ:full_moon_with_face:スモ:new_moon_with_face:スモ:full_moon_with_face:スモ:new_moon_with_face:スモ:full_moon_with_face:ス～～～モ:arrow_heading_down::sun_with_face:');
+        } catch (error) {
+            errorlog(client, message, error);
+        }
+        finally {
+            client.cooldown.set(message.author.id, false);
+        }
+    }
+}
