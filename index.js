b15114f591fd520388128f3374bc0dd75e25f89a
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2021-07-16 21:52:55 +0900

    Levelリーダーボード #60 簡易的なやつを作った

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 require('dotenv').config();
 const fs = require("fs");
 const cron = require('node-cron');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const http = require('http');
 const bot = require('./bot');
+const indexRouter = require('./routes/index');
 const client = new bot('outiserver.db');
+const app = express();
+const port = process.env.PORT || 3000;
 
+// Web関係設定
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', (req, res, next) => indexRouter(client, req, res, next));
+app.set('port', port);
+
+// 404
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// エラー
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const server = http.createServer(app);
+server.listen(port);
+
+// Processイベント読み込み
 fs.readdir(__dirname + "/events/process/", (err, files) => {
   if (err) return console.error(err);
   files.forEach((file) => {
@@ -14,6 +51,18 @@ fs.readdir(__dirname + "/events/process/", (err, files) => {
   });
 });
 
+// Serverイベント読み込み
+fs.readdir(__dirname + "/events/server/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/server/${file}`);
+    let eventName = file.split(".")[0];
+    server.on(eventName, event.bind(null, client))
+    console.log(`Server ${eventName} event is Loading`);
+  });
+});
+
+// Discordイベント読み込み
 fs.readdir(__dirname + "/events/discord/", (err, files) => {
   if (err) return console.error(err);
   files.forEach((file) => {
@@ -24,6 +73,18 @@ fs.readdir(__dirname + "/events/discord/", (err, files) => {
   });
 });
 
+// Cornイベント読み込み
+fs.readdir(__dirname + "/events/cron/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/cron/${file}`);
+    let eventTime = file.split(".")[0];
+    cron.schedule(eventTime, event.bind(null, client));
+    console.log(`time ${eventTime} event is Loading`);
+  });
+});
+
+// Command読み込み
 const commandFolders = fs.readdirSync('./commands');
 for (const folder of commandFolders) {
   const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
@@ -34,15 +95,7 @@ for (const folder of commandFolders) {
   }
 }
 
-fs.readdir(__dirname + "/events/cron/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    const event = require(__dirname + `/events/cron/${file}`);
-    let eventTime = file.split(".")[0];
-    cron.schedule(eventTime, event.bind(null, client));
-    console.log(`time ${eventTime} event is Loading`);
-  });
-});
+
 
 client.login()
   .catch(error => {
