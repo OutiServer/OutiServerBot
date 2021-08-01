0401ad629d568a4719e58412b13ca9101e000343
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2021-08-01 09:35:57 +0900

    復活のひ

--- events/cron/* * * * *.js
+++ events/cron/* * * * *.js
@@ -1,4 +1,5 @@
-const { Collection } = require('discord.js');
+const { Collection, MessageEmbed } = require('discord.js');
+const util = require('minecraft-server-util');
 const bot = require('../../bot');
 const { clienterrorlog } = require('../../functions/logs/error');
 
@@ -9,6 +10,33 @@ const { clienterrorlog } = require('../../functions/logs/error');
 module.exports = async (client) => {
     try {
         client.levelcooldown = new Collection();
+        const msg = await client.channels.cache.get('780012050163302420').messages.fetch('870272924974727209');
+        util.statusBedrock('126.235.33.140', { port: 19131, timeout: 5000 })
+            .then(async result => {
+                await msg.edit(
+                    new MessageEmbed()
+                        .setTitle('🏠おうちサーバー(PMMP)の現在の状態🏠')
+                        .addField('IPアドレス', result.host)
+                        .addField('ポート', result.port)
+                        .addField('サーバーのバージョン', result.version)
+                        .addField('デフォルトゲームモード', result.gameMode)
+                        .addField('現在参加中のメンバー', `${result.onlinePlayers}/${result.maxPlayers}人`)
+                        .setImage('https://media.discordapp.net/attachments/840154191036022794/840154293453062144/outisabakoiyo.png')
+                        .setColor('RANDOM')
+                        .setTimestamp()
+                );
+            })
+            .catch(async error => {
+                clienterrorlog(error);
+                await msg.edit(
+                    new MessageEmbed()
+                        .setTitle('🏠おうちサーバー(PMMP)の現在の状態🏠')
+                        .setDescription('おうちサーバー(PMMP)は現在落ちてます')
+                        .setImage('https://media.discordapp.net/attachments/840154191036022794/840154302605426698/setumeisitekudasai.png')
+                        .setColor('RANDOM')
+                        .setTimestamp()
+                );
+            })
     } catch (error) {
         clienterrorlog(error);
     }
