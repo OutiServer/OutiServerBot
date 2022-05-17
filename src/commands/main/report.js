840767606fcc574508420ff2633c281b45230baa
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-05-18 00:57:35 +0900

    レポートコマンドの追加

---
+++ src/commands/main/report.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Modal, MessageActionRow, TextInputComponent } = require('discord.js');
+
+module.exports = {
+    info: {
+        name: 'report',
+        description: 'バグを報告したり、Botに対する要望を開発者に送信するコマンド',
+        category: 'main',
+    },
+
+    data: new SlashCommandBuilder()
+        .setName('report')
+        .setDescription('バグを報告したり、Botに対する要望を開発者に送信するコマンド'),
+
+    /**
+     * @param {import('../../Bot')} client
+     * @param {import('discord.js').CommandInteraction} interaction
+     */
+
+    run: async function (client, interaction) {
+        const modal = new Modal()
+            .setCustomId('report')
+            .setTitle('Bot開発者へのバグ報告・要望');
+        modal.addComponents(new MessageActionRow()
+            .addComponents(new TextInputComponent()
+                .setCustomId('report_title')
+                .setLabel('タイトル')
+                .setStyle('SHORT'),
+            )
+            .addComponents(new TextInputComponent()
+                .setCustomId('report_content')
+                .setLabel('内容')
+                .setStyle('PARAGRAPH'),
+            ),
+        );
+
+        await interaction.showModal(modal);
+    },
+};
