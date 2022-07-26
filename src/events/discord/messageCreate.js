a6314b48b2f47be335e420080b28b2266ab473dc
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-07-27 00:48:58 +0900

    Oops

--- src/events/discord/messageCreate.js
+++ src/events/discord/messageCreate.js
@@ -7,8 +7,7 @@ const { default: axios } = require('axios');
  */
 
 module.exports = async (client, message) => {
-  if (message.channelId === '786343076389847040') {
-    console.log(`https://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${encodeURIComponent(message.content)}&source=en&target=ja`);
+  if (message.channelId === '786343076389847040' && message.author.id !== client.user.id) {
     const translationText = await axios.get(`https://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec?text=${encodeURIComponent(message.content)}&source=en&target=ja`);
     await message.reply(translationText.data);
   }
