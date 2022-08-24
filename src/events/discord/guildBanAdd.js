e7d9e32bd848db5d8bf53c5af5286f16060fd6f9
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-08-24 18:25:42 +0900

    ギルドチェックを忘れずに

--- src/events/discord/guildBanAdd.js
+++ src/events/discord/guildBanAdd.js
@@ -4,5 +4,6 @@
  */
 
 module.exports = async (client, ban) => {
+    if (ban.guild.id !== '706452606918066237') return;
     client.channels.cache.get('825231334657884161').send(`**Banned ${ban.user.tag}**\nReason: ${ban.reason ?? 'なし'}`);
 };
