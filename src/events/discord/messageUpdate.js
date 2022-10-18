3a0937b1c8cdc93ec6f2a738f705b7ede3390176
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-10-19 00:11:01 +0900

    使ってない機能全部消す

--- src/events/discord/messageUpdate.js
+++ src/events/discord/messageUpdate.js
@@ -14,47 +14,6 @@ module.exports = async (client, oldMessage, newMessage) => {
         newMessage = await newMessage.fetch();
     }
 
-    if (newMessage.author.id == '761562078095867916') {
-        if (newMessage.embeds[0].url == 'https://dissoku.net/' && newMessage.embeds[0].fields[0].name.endsWith('をアップしたよ!')) {
-            await newMessage.channel.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setDescription('upを確認しました、一時間後にこのチャンネルで通知します')
-                        .setTimestamp(),
-                ],
-            });
-            setTimeout(async () => {
-                await newMessage.channel.send('Upしてから一時間経ちました\n`/dissoku up` を実行しましょう');
-            }, 3600000);
-        }
-        else if (newMessage.embeds[0].url == 'https://dissoku.net/' && newMessage.embeds[0].fields[0].value.startsWith('間隔をあけてください')) {
-            const waittime_up = newMessage.embeds[0].fields[0].value.split('間隔をあけてください')[1].split('(')[1].split(')')[0];
-            await newMessage.channel.send(`Upに失敗したようです、${waittime_up}後にもう一度もう一度実行してください！`);
-        }
-    }
-    else if (newMessage.author.id == '302050872383242240') {
-        if (newMessage.embeds[0].url == 'https://disboard.org/' && (newMessage.embeds[0].description.match(/表示順をアップしたよ/))) {
-            newMessage.channel.send(
-                {
-                    embeds: [
-                        new EmbedBuilder()
-                            .setDescription('Bumpを確認しました、二時間後にこのチャンネルで通知します')
-                            .setTimestamp(),
-                    ],
-                },
-            );
-            setTimeout(async () => {
-                await newMessage.channel.send('Bumpしてから二時間経ちました\n`!d bump` を実行しましょう');
-            }, 7200000);
-        }
-        else if (newMessage.embeds[0].url == 'https://disboard.org/' && (newMessage.embeds[0].description.match(/このサーバーを上げられるようになるまで/))) {
-            const waittime_bump = newMessage.embeds[0].description.split('と')[1].split('分')[0];
-            newMessage.channel.send({
-                content: `Bumpに失敗したようです、${waittime_bump}分後にもう一度もう一度実行してください！`,
-            });
-        }
-    }
-
     if (oldMessage.author.bot || oldMessage.system || oldMessage.guildId !== '706452606918066237') return;
     const embed = new EmbedBuilder()
         .setTitle('メッセージが編集されました')
