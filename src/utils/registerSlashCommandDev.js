8c09e2148a58f72eec9a5ed0b8c46cef16132dfb
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-10-21 01:52:15 +0900

    fix

---
+++ src/utils/registerSlashCommandDev.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { readdirSync } = require('fs');
+const commands = [];
+
+const commandFolders = readdirSync('./src/commands');
+for (const folder of commandFolders) {
+    const commandFiles = readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`../commands/${folder}/${file}`);
+        if (!command.data) continue;
+        commands.push(command.data);
+        console.log(`${command.info.name} command is Added`);
+    }
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, '872880984205430834'),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    }
+    catch (error) {
+        console.error(error);
+    }
+})();
