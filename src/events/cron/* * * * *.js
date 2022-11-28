7bfc9f908ede8a8cd962d9edc0ae61dabac2fbcf
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-11-28 16:57:52 +0900

    add status

--- src/events/cron/* * * * *.js
+++ src/events/cron/* * * * *.js
@@ -1,5 +1,6 @@
 const { userMention } = require('@discordjs/builders');
 const { EmbedBuilder, SnowflakeUtil } = require('discord.js');
+const util = require('minecraft-server-util');
 
 /**
  * @param {import('../../Bot')} client
@@ -74,4 +75,36 @@ module.exports = async (client) => {
             catch (e) {
             }
         });
+
+    const statusMsg = await client.channels.cache.get('1046694435829858395').messages.fetch('1046694662183854130');
+    try {
+        const status = await util.status('outiserver.com', 25565, { timeout: 5000, enableSRV: true });
+
+        await statusMsg.edit({
+            content: '',
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle('おうちサーバーの状態')
+                    .addFields([
+                        { name: 'プレイヤー数', value: `${status.players.online}/${status.players.max}\n\n参加中のプレイヤーリスト\n${status.players.sample.map(p => `${p.name}`).join('\n')}` },
+                        { name: '参加中のプレイヤーリスト', value: status.players.sample.map(p => `${p.name}`).join('\n') },
+                        { name: 'IP', value: 'outiserver.com' },
+                        { name: 'Port', value: 'Java Edition: 25565\nBedrock Edition: 50001' },
+                        { name: 'Version', value: status.version.name },
+                    ])
+                    .setImage('https://media.discordapp.net/attachments/906452841890205756/954540860224184341/outisabakoiyo.jpeg'),
+            ],
+        });
+    }
+    catch (e) {
+        await statusMsg.edit({
+            content: '',
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle('おうちサーバーの状態')
+                    .setDescription('おうちサーバーは現在落ちています')
+                    .setImage('https://media.discordapp.net/attachments/906452841890205756/954540727898099732/setumeisitekudasai.jpeg'),
+            ],
+        });
+    }
 };
