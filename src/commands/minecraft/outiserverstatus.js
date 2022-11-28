b3e6279f907c9fdb8fe00a923e1a417e59b3e67c
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-11-28 16:49:51 +0900

    fix outiserverstatus

--- src/commands/minecraft/outiserverstatus.js
+++ src/commands/minecraft/outiserverstatus.js
@@ -22,12 +22,10 @@ module.exports = {
      */
 
     run: async function (client, interaction) {
-        if (interaction.guildId !== '877587515677237258' && interaction.guildId !== '872880984205430834') return await interaction.followUp('このコマンドはこのサーバーでは使用できません');
-
         const msg = await interaction.followUp('サーバー状態を取得中です、お待ちください...');
 
         try {
-            const status = await util.statusBedrock('126.21.60.218', 50000, { timeout: 5000 });
+            const status = await util.status('outiserver.com', 25565, { timeout: 5000, enableSRV: true });
 
             msg.edit({
                 content: 'ステータス取得に成功しました',
@@ -35,9 +33,9 @@ module.exports = {
                     new EmbedBuilder()
                         .setTitle('おうちサーバーの状態')
                         .addFields([
-                            { name: 'プレイヤー数', value: `${status.players.online}/${status.players.max}` },
-                            { name: 'IP', value: '126.21.60.218' },
-                            { name: 'Port', value: '50000' },
+                            { name: 'プレイヤー数', value: `${status.players.online}/${status.players.max}\n\n参加中のプレイヤーリスト\n${status.players.sample.map(p => `${p.name}(${p.id})`).join('\n')}` },
+                            { name: 'IP', value: 'outiserver.com' },
+                            { name: 'Port', value: 'Java Edition: 25565\nBedrock Edition: 50001' },
                             { name: 'Version', value: status.version.name },
                         ])
                         .setImage('https://media.discordapp.net/attachments/906452841890205756/954540860224184341/outisabakoiyo.jpeg'),
